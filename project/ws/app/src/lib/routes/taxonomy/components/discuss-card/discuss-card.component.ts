e68ec0fdc52978d74095d7c9d99c2b64c5299576
Jenifer Ramsingh <jenifer.ramsingh@tarento.com>
2021-04-20 19:35:32 +0530

    taxonomy feature folder added

---
+++ project/ws/app/src/lib/routes/taxonomy/components/discuss-card/discuss-card.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { NSDiscussData } from '../../models/discuss.model'
+@Component({
+  selector: 'app-dicuss-card',
+  templateUrl: './discuss-card.component.html',
+  styleUrls: ['./discuss-card.component.scss'],
+  /* tslint:disable */
+  host: { class: 'flex flex-1 margin-top-l' },
+  /* tslint:enable */
+})
+
+export class DiscussCardComponent implements OnInit {
+  @Input()
+  discuss!: NSDiscussData.IDiscussionData
+  @Input()
+  tags!: any
+  rem!: number
+  @Output() clickedTab = new EventEmitter<string>()
+
+  showRem = true
+  public static get MINIMUM_LENGTH(): number { return 8 }
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    if (this.tags && this.tags.length > DiscussCardComponent.MINIMUM_LENGTH) {
+    this.rem = this.calculateLength(this.tags.length)
+    this.tags.length =  DiscussCardComponent.MINIMUM_LENGTH
+    }
+   }
+  upvote(discuss: NSDiscussData.IDiscussionData) {
+    if (discuss) {
+
+    }
+
+  }
+  downvote(discuss: NSDiscussData.IDiscussionData) {
+    if (discuss) {
+
+    }
+  }
+  getClickedTab(tab: string) {
+    this.clickedTab.emit(tab)
+  }
+  getDiscussion() {
+    // this.router.navigate([`/app/discuss/home/${this.discuss.tid}`])
+    // this.tags=["2nd Level Topic",  "2nd Level Topic", "2nd Level Topic","small",
+    // "2nd Level Topic with large", "2nd Level Topic very large","2nd Level Topic","2nd Level Topic with Extra large",
+    // "2nd Level", "2nd Level Topic","2nd Level Topic","small",
+    // "2nd Level Topic with large", "2nd Level Topic very large","2nd Level Topic","2nd Level Topic with Extra large",
+    // "2nd Level Topic with large", "2nd Level Topic very large","2nd Level Topic","2nd Level Topic with Extra large" ]
+    this.showRem = false
+  }
+
+  calculateLength(len: number) {
+    return len - DiscussCardComponent.MINIMUM_LENGTH
+  }
+}
---
+++ project/ws/app/src/lib/routes/taxonomy/routes/discuss-topics/discuss-topics.component.ts
@@ -0,0 +1,218 @@
+
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { ActivatedRoute, Router, Event, NavigationEnd, NavigationError } from '@angular/router'
+import { ValueService } from '@sunbird-cb/utils'
+import { map } from 'rxjs/operators'
+import { NsWidgetResolver } from '@sunbird-cb/resolver'
+import { TaxonomyService } from '../../services/taxonomy.service'
+const APP_TAXONOMY = `/app/taxonomy/`
+@Component({
+  selector: 'app-discuss',
+  templateUrl: './discuss-topics.component.html',
+  styleUrls: ['./discuss-topics.component.scss'],
+})
+export class DiscussTopicsComponent implements OnInit, OnDestroy {
+  sideNavBarOpened = true
+  panelOpenState = false
+  nextLevelTopic: any
+  firstLevelTopic: any
+  currentTab: any
+  titles = [{ title: 'DISCUSS', url: '/app/discuss/home', icon: 'forum' }]
+  relatedResource: any = []
+  unread = 0
+  currentObj!: any
+  nextLvlObj!: any
+  leftMenuChildObj!: any
+  currentRoute = 'home'
+  breadCrum!:any
+  banner!: NsWidgetResolver.IWidgetData<any>
+  public screenSizeIsLtMedium = false
+  isLtMedium$ = this.valueSvc.isLtMedium$
+  mode$ = this.isLtMedium$.pipe(map(isMedium => (isMedium ? 'over' : 'side')))
+  private defaultSideNavBarOpenedSubscription: any
+  isFirst = true
+  constructor(private valueSvc: ValueService, private route: ActivatedRoute, private router: Router,  private _service:  TaxonomyService) {
+    this.unread = this.route.snapshot.data.unread
+
+    this.currentTab = this.route.snapshot.url.toString().split('/').pop()
+    this.getAllTopics(this.currentTab)
+    this. getAllRelatedCourse()
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+        // console.log(event.url)
+
+      }
+
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+
+        // Present error to user
+        // console.log(event.error)
+      }
+    })
+    // this.bannerSubscription = this.route.data.subscribe(data => {
+    //   if (data && data.pageData) {
+    //     this.banner = data.pageData.data.banner || []
+    //   }
+    // })
+  }
+  ngOnInit() {
+    this.defaultSideNavBarOpenedSubscription = this.isLtMedium$.subscribe(isLtMedium => {
+      this.sideNavBarOpened = !isLtMedium
+      this.screenSizeIsLtMedium = isLtMedium
+    })
+    this.breadCrum = {titles:[{title:'Home', url:'/app/taxonomy/home'},{title:'Law',url:'none'}, {title:'Law',url:'none'}, {title:'Law',url:'none'}]}
+    // this.firstLevelTopic =  [{name: "Economics", enabled: true, routerLink:"/app/taxonomy/test"},
+    // {name: "1st level  topic", enabled: true, routerLink:"/app/taxonomy/116"},
+    // {name: "1st level  topic", enabled: true, routerLink:"/app/taxonomy/ll1"},
+    // {name: "1st level  topic", enabled: true,  routerLink:"/app/taxonomy/ll2"},
+    // {name: "1st level  topic", enabled: true, routerLink:"/app/taxonomy/ll3"},
+    // {name: "1st level  topic", enabled: true, routerLink:"/app/taxonomy/ll4"},
+    // {name: "1st level  topic", enabled: true,routerLink:"/app/taxonomy/ll5"},]
+
+    // this.nextLevelTopic = ["2nd Level Topic",  "2nd Level Topic", "2nd Level Topic","small",
+    // "2nd Level Topic with large", "2nd Level Topic very large","2nd Level Topic","2nd Level Topic with Extra large",
+    // "2nd Level", "2nd Level Topic","2nd Level Topic","small",
+    // "2nd Level Topic with large", "2nd Level Topic very large","2nd Level Topic","2nd Level Topic with Extra large",
+    // "2nd Level Topic with large", "2nd Level Topic very large","2nd Level Topic","2nd Level Topic with Extra large" ]
+
+  }
+  ngOnDestroy() {
+    if (this.defaultSideNavBarOpenedSubscription) {
+      this.defaultSideNavBarOpenedSubscription.unsubscribe()
+    }
+    // if (this.bannerSubscription) {
+    //   this.bannerSubscription.unsubscribe()
+    // }
+  }
+  getAllTopics(topic: string) {
+    this._service.fetchAllTopics().subscribe(response => {
+      this.currentObj = response.terms
+      this.nextLvlObj = response.terms
+      this.dataProcess(topic)
+      })
+    }
+    dataProcess(topic: string) {
+      const firstLvlArray: any[] = []
+      const tempCurrentArray: any[] = []
+      this.currentObj.forEach((term: any) => {
+        // if(term.name!==decodeURI(topic)){
+          const obj = {
+            name: term.name,
+            enabled: true,
+            routerLink: APP_TAXONOMY + term.name,
+          }
+          firstLvlArray.push(obj)
+          this.currentTab = term.name
+            this.firstLevelTopic = firstLvlArray
+            if (term.name === decodeURI(topic) && term.children) {
+
+              const nextLevel: string[] = []
+              term.children.forEach((second: any) => {
+                nextLevel.push(second.name)
+                tempCurrentArray.push(second)
+              })
+
+              this.nextLevelTopic = nextLevel
+            }
+
+        })
+        this.nextLvlObj = tempCurrentArray
+    }
+    dataProcessOn2ndLevel(topic: string) {
+      const tempCurrentArray: any[] = []
+      this.nextLevelTopic = []
+      if (this.leftMenuChildObj) {
+      this.leftMenuChildObj.forEach((term: any) => {
+          this.currentTab = term.name
+            if (term.name === topic && term.children) {
+              this.nextLvlObj = term.children
+              const nextLevel: string[] = []
+              term.children.forEach((second: any) => {
+                nextLevel.push(second.name)
+                tempCurrentArray.push(second)
+              })
+              this.nextLevelTopic = nextLevel
+            }
+
+        })
+      }
+        // this.nextLvlObj = tempCurrentArray
+    }
+    selectedEvent(tabItem: string) {
+      if (this.isFirst) {
+        this.dataProcess(tabItem)
+      } else {
+        this.dataProcessOn2ndLevel(tabItem)
+      }
+
+    }
+    getClickedTab(clickedTab: string) {
+      this.isFirst = false
+      const leftMenuData: any[] = []
+      this.nextLvlObj.forEach((term: any) => {
+        leftMenuData.push(term)
+        if (term.name === decodeURI(clickedTab)) {
+          this.dataProcessOneMore(clickedTab, this.nextLvlObj)
+        }
+        })
+        this.leftMenuChildObj = leftMenuData
+    }
+
+    dataProcessOneMore(topic: string, processObj: any) {
+      const firstLvlArray: any[] = []
+      this.firstLevelTopic = []
+      this.nextLevelTopic = []
+      processObj.forEach((term: any) => {
+        if (term) {
+        if (term.name === decodeURI(topic)) {
+          if (term.children) {
+          this.nextLvlObj = term.children
+          }
+        }
+          const obj = {
+            name: term.name,
+            enabled: true,
+            routerLink: APP_TAXONOMY + term.name,
+          }
+          firstLvlArray.push(obj)
+        // } else {
+          // const firstObj = {
+          //   name: decodeURI(topic),
+          //   enabled: true,
+          //   routerLink: APP_TAXONOMY + decodeURI(topic),
+          // }
+
+          // firstLvlArray.splice(0, 0, firstObj)
+          this.router.navigate([APP_TAXONOMY + decodeURI(topic)])
+        // }
+
+            if (term.name === topic && term.children) {
+              const nextLevel: string[] = []
+              term.children.forEach((second: any) => {
+                nextLevel.push(second.name)
+              })
+              if (nextLevel.length > 0) {
+                this.nextLevelTopic = nextLevel
+              }
+
+            }
+          }
+        })
+        this.firstLevelTopic = firstLvlArray
+
+    }
+  getAllRelatedCourse() {
+      this._service.fetchAllRelatedCourse().subscribe(response => {
+        const tempRequestParam: { content: any }[] = []
+        response.result.content.forEach((course: any) => {
+         const temobj = {
+           content: course,
+         }
+         tempRequestParam.push(temobj)
+        })
+        this.relatedResource = tempRequestParam
+      })
+    }
+}
