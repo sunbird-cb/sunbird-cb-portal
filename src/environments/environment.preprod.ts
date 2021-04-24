c438be954a678eb92f3131297d3131ed8aee2ef9
christopher.fernandes <christyabcd.05@gmail.com>
2021-04-24 21:08:06 +0530

    Task #54141: Fixed the sitepath for preprod

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  sitePath: 'eagle-sb.idc.tarento.com',
+  sitePath: 'igot-pp-sunbird.idc.tarento.com',
   organisation: 'igot-karmayogi',
   framework: 'igot',
   channelId: '0131397178949058560',
