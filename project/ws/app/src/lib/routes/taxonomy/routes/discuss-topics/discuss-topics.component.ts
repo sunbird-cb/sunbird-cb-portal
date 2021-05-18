ea6f44e66d1a2d476ca489d5f62ca50eeb83fa56
Jenifer Ramsingh <jenifer.ramsingh@tarento.com>
2021-05-18 15:05:55 +0530

    code cleaning

--- project/ws/app/src/lib/routes/taxonomy/routes/discuss-topics/discuss-topics.component.ts
+++ project/ws/app/src/lib/routes/taxonomy/routes/discuss-topics/discuss-topics.component.ts
@@ -68,7 +68,7 @@ export class DiscussTopicsComponent implements OnInit, OnDestroy {
       this.sideNavBarOpened = !isLtMedium
       this.screenSizeIsLtMedium = isLtMedium
     })
-    this.tempArr  =  [{ title: 'Home', url: '/app/taxonomy/home' }]
+    this.tempArr  =  [{ title: 'All topics', url: '/app/taxonomy/home' }]
     this.alreadyClicked = true
     // this.firstLevelTopic =  [{name: "Economics", enabled: true, routerLink:"/app/taxonomy/test"},
     // {name: "1st level  topic", enabled: true, routerLink:"/app/taxonomy/116"},
