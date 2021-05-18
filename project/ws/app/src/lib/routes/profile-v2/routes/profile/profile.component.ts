6fb073f86c554d556644731d840a0812b8d5f577
NandiniAV <nandini.achutha@tarento.com>
2021-05-18 11:54:52 +0530

    Fixed profile page breadcrumd username issue

--- project/ws/app/src/lib/routes/profile-v2/routes/profile/profile.component.ts
+++ project/ws/app/src/lib/routes/profile-v2/routes/profile/profile.component.ts
@@ -1,9 +1,10 @@
 
 import { Component, OnInit, OnDestroy } from '@angular/core'
 import { Router, Event, NavigationEnd, NavigationError, ActivatedRoute } from '@angular/router'
-import { ValueService } from '@sunbird-cb/utils'
+import { ValueService, ConfigurationsService } from '@sunbird-cb/utils'
 import { map } from 'rxjs/operators'
 import { NsWidgetResolver } from '@sunbird-cb/resolver'
+
 /* tslint:disable */
 import _ from 'lodash'
 /* tslint:enable */
@@ -29,36 +30,46 @@ export class ProfileComponent implements OnInit, OnDestroy {
   mode$ = this.isLtMedium$.pipe(map(isMedium => (isMedium ? 'over' : 'side')))
   userRouteName = ''
   private defaultSideNavBarOpenedSubscription: any
-  constructor(private valueSvc: ValueService, private router: Router, private activeRoute: ActivatedRoute) {
+  constructor(private valueSvc: ValueService, private router: Router,
+              private activeRoute: ActivatedRoute,
+              private configSvc: ConfigurationsService) {
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
-        // Hide loading indicator
-        // console.log(event.url)
         this.bindUrl(event.urlAfterRedirects.replace('/app/discuss/', ''))
-      }
 
-      if (event instanceof NavigationError) {
-        // Hide loading indicator
-
-        // Present error to user
-        // console.log(event.error)
+        if (event.urlAfterRedirects === '/app/person-profile/me') {
+          if (this.configSvc.userProfile) {
+            this.userRouteName = `${this.configSvc.userProfile.firstName} ${this.configSvc.userProfile.lastName}`
+            this.titles = [{ title: 'NETWORK', url: '/app/network-v2', icon: 'group' }]
+            if (this.userRouteName && this.userRouteName.trim()) {
+              this.titles.push({
+                icon: '',
+                title: `${this.userRouteName}\'profile`,
+                url: 'none',
+              })
+            }
+          }
+        } else {
+          if (this.activeRoute.firstChild) {
+            this.activeRoute.firstChild.data.subscribe(response => {
+              this.userRouteName = response && response.profile && response.profile.data && response.profile.data[0]
+                && response.profile.data[0].personalDetails &&
+                `${(response.profile.data[0].personalDetails.firstname || '')} ${(response.profile.data[0].personalDetails.surname)}`
+            })
+            this.titles = [{ title: 'NETWORK', url: '/app/network-v2', icon: 'group' }]
+            if (this.userRouteName && this.userRouteName.trim()) {
+              this.titles.push({
+                icon: '',
+                title: `${this.userRouteName}\'profile`,
+                url: 'none',
+              })
+            }
+          }
+        }
       }
     })
-    if (this.activeRoute.firstChild) {
-      this.activeRoute.firstChild.data.subscribe(response => {
-        this.userRouteName = response && response.profile && response.profile.data && response.profile.data[0]
-          && response.profile.data[0].personalDetails &&
-          `${(response.profile.data[0].personalDetails.firstname || '')} ${(response.profile.data[0].personalDetails.surname)}`
-      })
-      if (this.userRouteName && this.userRouteName.trim()) {
-        this.titles.push({
-          icon: '',
-          title: `${this.userRouteName}\'profile`,
-          url: 'none',
-        })
-      }
-    }
   }
+
   ngOnInit() {
     this.defaultSideNavBarOpenedSubscription = this.isLtMedium$.subscribe(isLtMedium => {
       this.sideNavBarOpened = !isLtMedium
