1b9a166e3551802de631e096d436c65225156c90
Jenifer Ramsingh <jenifer.ramsingh@tarento.com>
2021-05-19 11:29:10 +0530

    unwanted component removed

--- project/ws/app/src/lib/routes/taxonomy/taxonomy.module.ts
+++ project/ws/app/src/lib/routes/taxonomy/taxonomy.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { PipeFilterModule, PipeHtmlTagRemovalModule, PipeOrderByModule, PipeRelativeTimeModule, PipeListFilterModule } from '@sunbird-cb/utils'
 import { TaxonomyHomeComponent } from './routes/taxonomy-home/taxonomy.component'
-import { DiscussCommetsComponent } from './components/discuss-comments/discuss-comments.component'
 import { TaxonomyRoutingModule } from './taxonomy.rounting.module'
 import { TaxonomyLevelCardComponent } from './components/taxonomy-level-card/taxonomy-level-card.component'
-import { CategoryCardComponent } from './components/category-card/category-card.component'
 import { LeftMenuComponent } from './components/left-menu/left-menu.component'
-import { PostCardComponent } from './components/post-card/post-card.component'
-import { RightMenuComponent } from './components/right-menu/right-menu.component'
+
 // import { BasicCKEditorComponent } from './components/basic-ckeditor/basic-ckeditor.component'
 import { MatGridListModule } from '@angular/material/grid-list'
 import { MatExpansionModule } from '@angular/material/expansion'
@@ -29,28 +26,20 @@ import {
 import { MatCardModule } from '@angular/material/card'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { TrendingTagsComponent } from './components/trending-tags/trending-tags.component'
-import { RelatedDiscussionComponent } from './components/related-discussion/related-discussion.component'
 import { AvatarPhotoModule, BtnPageBackModule, CardContentModule, BreadcrumbsOrgModule } from '@sunbird-cb/collection'
 import { EditorSharedModule } from '@ws/author/src/lib/routing/modules/editor/shared/shared.module'
 // import { CkEditorModule } from 'library/ws-widget/collection/src/lib/_common/ck-editor/ck-editor.module'
 import { LoaderService } from '@ws/author/src/lib/services/loader.service'
 // import { CKEditorService } from 'library/ws-widget/collection/src/lib/_common/ck-editor/ck-editor.service'
-import { PaginationComponent } from './components/pagination/pagination.component'
 
 import { DiscussTopicsComponent } from './routes/discuss-topics/discuss-topics.component'
 
 @NgModule({
   declarations: [
-    CategoryCardComponent,
     TaxonomyHomeComponent,
     TaxonomyLevelCardComponent,
-    DiscussCommetsComponent,
     LeftMenuComponent,
-    PostCardComponent,
-    RightMenuComponent,
-    RelatedDiscussionComponent,
     TrendingTagsComponent,
-    PaginationComponent,
     DiscussTopicsComponent,
   ],
   imports: [
@@ -90,9 +79,6 @@ import { DiscussTopicsComponent } from './routes/discuss-topics/discuss-topics.c
   providers: [
     // CKEditorService,
     LoaderService,
-  ],
-  exports: [
-    PostCardComponent,
   ],
 })
 export class TaxonomyModule {
