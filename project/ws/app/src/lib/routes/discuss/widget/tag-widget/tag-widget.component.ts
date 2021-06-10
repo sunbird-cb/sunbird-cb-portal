b592b64cc681790b43fa16b569404d21cf17bb23
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-06-10 11:41:01 +0530

    Changes for tags-widget

--- project/ws/app/src/lib/routes/discuss/widget/tag-widget/tag-widget.component.ts
+++ project/ws/app/src/lib/routes/discuss/widget/tag-widget/tag-widget.component.ts
@@ -16,6 +16,8 @@ export class TagWidgetComponent extends BaseWrapperComponent {
   homePage = 'categoryHome'
   tid!: number
   slug!: string
+  previousSlug: any
+  stateCount = 0
   context: any = { categories: { result: [] } }
   categoryId: any
   alldiscussPage = 'alldiscuss'
@@ -35,17 +37,30 @@ export class TagWidgetComponent extends BaseWrapperComponent {
   // @Input() config
 
   wrapperInit() {
+    // debugger
     this.state = this.alldiscussPage
   }
 
   widgetBackClick() {
-    this.state = this.alldiscussPage
+    this.stateCount = this.stateCount - 1
+    if (this.stateCount === 0 || this.stateCount === 1) {
+      this.state = this.alldiscussPage
+      this.previousSlug = ''
+    } else {
+      this.state = this.previousState
+    }
+
+    if (this.previousSlug) {
+      this.slug = this.previousSlug
+      this.state = this.tagAllDiscussPage
+    }
   }
 
   stateChange(event: any) {
-    // debugger
     // console.log(event)
     this.previousState = this.state
+    // tslint:disable-next-line: no-increment-decrement
+    this.stateCount++
     this.state = event.action
     if (event.action === this.detailsPage) {
       this.tid = event.tid
@@ -55,6 +70,7 @@ export class TagWidgetComponent extends BaseWrapperComponent {
     if (event.action === this.tagAllDiscussPage) {
       this.tid = event.tid
       this.slug = event.title
+      this.previousSlug = event.title
     }
   }
 }
