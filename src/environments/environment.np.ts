69eb86c8ca1f0e4ed2f34d06a7bf1be0853c844e
Amit Kumar <amit.sengar@tarento.com>
2021-06-17 12:51:18 +0530

    NIC prod env added

---
+++ src/environments/environment.np.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    sitePath: 'karmayogiprod.nic.in',
+    organisation: 'igot-karmayogi',
+    framework: 'igot',
+    channelId: '0131397178949058560',
+    azureHost: 'https://karmayogi.nic.in',
+    contentHost: 'https://content.igot-dev.in',
+    azureBucket: 'content-store',
+  
+    azureOldHost: 'https://staas-bbs1.cloud.gov.in',
+    azureOldBuket: 'igot',
+  }
+  
