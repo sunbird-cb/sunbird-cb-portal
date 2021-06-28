43f6a33120e5d23902bb433df6fc170c5e5ee97f
NandiniAV <nandini.achutha@tarento.com>
2021-06-28 19:13:17 +0530

    Searchv2 filter integration & UI

--- project/ws/app/src/lib/routes/search-v2/routes/learn-search/learn-search.component.ts
+++ project/ws/app/src/lib/routes/search-v2/routes/learn-search/learn-search.component.ts
@@ -14,22 +14,25 @@ export class LearnSearchComponent implements OnInit {
   searchRequestObject = {
     request: {
       filters: {
-        visibility: ['Default'],
-        contentType: [
-          'Course',
-          'Learning Resource',
-          'Resource',
-        ],
-        primaryCategory: ['Course', 'Learning Resource'],
+        contentType: [],
+        primaryCategory: [],
+        mimeType: [],
+        source: [],
       },
       query: '',
       sort_by: { lastUpdatedOn: '' },
       fields: [],
-      facets: ['primaryCategory', 'mimeType'],
+      facets: [],
     },
   }
   totalResults: any
   defaultThumbnail = ''
+  myFilters = []
+  rfilter: any
+  primaryCategoryType: any = []
+  mimeType: any = []
+  sourceType: any = []
+  facets: any = []
 
   constructor(private searchSrvc: GbSearchService, private configSvc: ConfigurationsService, private router: Router) { }
 
@@ -38,28 +41,82 @@ export class LearnSearchComponent implements OnInit {
     if (instanceConfig) {
       this.defaultThumbnail = instanceConfig.logos.defaultContent || ''
     }
-    this.searchRequestObject.request.query = this.param
     this.getSearchedData()
   }
 
   getSearchedData() {
-    const queryparam = this.searchRequestObject
+    const queryparam = {
+      request: {
+        filters: {
+          visibility: ['Default'],
+          contentType: [
+            'Course',
+            'Learning Resource',
+            'Resource',
+          ],
+          primaryCategory: ['Course', 'Learning Resource'],
+        },
+        query: this.param,
+        sort_by: { lastUpdatedOn: '' },
+        fields: [],
+        facets: ['primaryCategory', 'mimeType', 'mediaType', 'source'],
+      },
+    }
     this.searchSrvc.fetchSearchData(queryparam).subscribe((response: any) => {
       this.searchResults = response.result.content
       this.totalResults = response.result.count
+      this.facets = response.result.facets
     })
   }
 
   viewContent(content: any) {
     // const url = `/app/toc/${content.identifier}/overview?primaryCategory=${content.primaryCategory}`
-    // this.router.navigate([url])
 
-    this.router.navigate([`/app/toc/${content.identifier}/overview?primaryCategory=/${content.primaryCategory}`])
+    // this.router.navigate([`/app/toc/${content.identifier}/overview?primaryCategory=/${content.primaryCategory}`])
+    let url = `/app/toc/${content.identifier}/overview`
+    url += content.primaryCategory ? `?primaryCategory=/${content.primaryCategory}` : ''
+    this.router.navigate([url])
 
     // this.router.navigate([
     //   `${forPreview ? '/author' : '/app'}/toc/${resolveData.data.identifier}/${
     //   resolveData.data.children.length ? 'contents' : 'overview'
     //   }?primaryCategory=${resolveData.data.primaryCategory}`,
     // ])
+  }
+
+  applyFilter(filter: any) {
+    if (filter && filter.length > 0) {
+      this.myFilters = filter
+      const queryparam = this.searchRequestObject
+      queryparam.request.filters.primaryCategory = []
+      queryparam.request.filters.mimeType = []
+      queryparam.request.filters.source = []
+      this.myFilters.forEach((mf: any) => {
+        queryparam.request.query = this.param
+        if (mf.mainType === 'primaryCategory') {
+          this.primaryCategoryType.push(mf.name)
+          queryparam.request.filters.primaryCategory = this.primaryCategoryType
+        } else if (mf.mainType === 'mimeType') {
+          this.mimeType.push(mf.name)
+          queryparam.request.filters.mimeType = this.mimeType
+        } else if (mf.mainType === 'source') {
+          this.sourceType.push(mf.name)
+          queryparam.request.filters.source = this.sourceType
+        }
+      })
+      this.searchSrvc.fetchSearchData(queryparam).subscribe((response: any) => {
+        this.searchResults = response.result.content
+        this.totalResults = response.result.count
+        this.primaryCategoryType = []
+        this.mimeType = []
+        this.sourceType = []
+      })
+    } else {
+      this.getSearchedData()
+    }
+  }
+
+  removeFilter (mfilter: any) {
+    this.rfilter = mfilter
   }
 }
