68d2385222b3cffcfc840dfad9bb058965175b4f
gohilamariappan <41056032+gohilamariappan@users.noreply.github.com>
2021-07-07 19:58:00 +0530

    build.sh added

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Build script
+set -eo pipefail
+
+build_tag=$1
+name='ui-static'
+node=$2
+org=$3
+
+docker build -f ${dockerfile} --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
