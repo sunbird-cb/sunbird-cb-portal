9bd8f716abb9a0a2663913b0cac252cbcdb24dee
NandiniAV <nandini.achutha@tarento.com>
2021-08-17 15:59:27 +0530

    Updated events apis & issues fixed

--- project/ws/app/src/lib/routes/events/services/events.service.ts
+++ project/ws/app/src/lib/routes/events/services/events.service.ts
@@ -1,40 +1,23 @@
 import { Injectable } from '@angular/core'
-import { HttpBackend, HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 
 const API_END_POINTS = {
-  EVENT_READ: `/api/event/v4/read`,
-  GET_EVENTS: '/api/composite/v1/search',
+  EVENT_READ: `/apis/proxies/v8/api/event/v4/read`,
+  GET_EVENTS: '/apis/proxies/v8/sunbirdigot/search',
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventService {
-    private newHttp: HttpClient
-    private newHttp1: HttpClient
-    constructor(handler: HttpBackend, handler1: HttpBackend) {
-        this.newHttp = new HttpClient(handler)
-        this.newHttp1 = new HttpClient(handler1)
-    }
+  constructor(private http: HttpClient) {}
 
-    getEventData(eventId: any): Observable<any> {
-        const options = {
-        headers: {
-            // tslint:disable-next-line:max-line-length
-            Authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJRekw4VVA1dUtqUFdaZVpMd1ZtTFJvNHdqWTg2a2FrcSJ9.TPjV0xLacSbp3FbJ7XeqHoKFN35Rl4YHx3DZNN9pm0o',
-        },
-        }
-        return this.newHttp.get<any>(`${API_END_POINTS.EVENT_READ}/${eventId}`, options)
-    }
+  getEventData(eventId: any): Observable<any> {
+      return this.http.get<any>(`${API_END_POINTS.EVENT_READ}/${eventId}`)
+  }
 
-    getEventsList(req: any) {
-        const options = {
-          headers: {
-            // tslint:disable-next-line:max-line-length
-            Authorization: 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJRekw4VVA1dUtqUFdaZVpMd1ZtTFJvNHdqWTg2a2FrcSJ9.TPjV0xLacSbp3FbJ7XeqHoKFN35Rl4YHx3DZNN9pm0o',
-          },
-        }
-        return this.newHttp1.post<any>(`${API_END_POINTS.GET_EVENTS}`, req, options)
-      }
+  getEventsList(req: any) {
+      return this.http.post<any>(`${API_END_POINTS.GET_EVENTS}`, req)
+    }
 }
