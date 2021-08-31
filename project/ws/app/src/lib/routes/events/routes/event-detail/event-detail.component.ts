ddaba29c11e6600d6e8edea607cd09564db20d81
NandiniAV <nandini.achutha@tarento.com>
2021-08-31 12:03:13 +0530

    Added condition for event status display

--- project/ws/app/src/lib/routes/events/routes/event-detail/event-detail.component.ts
+++ project/ws/app/src/lib/routes/events/routes/event-detail/event-detail.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog'
 // import { DiscussService } from '../../../discuss/services/discuss.service'
 /* tslint:disable */
 import _ from 'lodash'
+import * as moment from 'moment'
 import { EventService } from '../../services/events.service'
 /* tslint:enable */
 
@@ -23,6 +24,7 @@ export class EventDetailComponent implements OnInit {
   eventId!: any
   fetchSingleCategoryLoader = false
   eventData: any
+  currentEvent = false
   // fetchNewData = false
 
   constructor(
@@ -41,12 +43,48 @@ export class EventDetailComponent implements OnInit {
       // }
       // this.data = this.route.snapshot.data.topic.data
     })
-     this.eventSvc.getEventData(this.eventId).subscribe((data: any) => {
-        this.eventData = data.result.event
-        const creatordata = this.eventData.creatorDetails
-        const str = creatordata.replace(/\\/g, '')
-        this.eventData.creatorDetails = JSON.parse(str)
-      })
+    this.eventSvc.getEventData(this.eventId).subscribe((data: any) => {
+      this.eventData = data.result.event
+      const creatordata = this.eventData.creatorDetails
+      const str = creatordata.replace(/\\/g, '')
+      this.eventData.creatorDetails = JSON.parse(str)
+      const eventDate = this.customDateFormat(this.eventData.startDate, this.eventData.startTime)
+      const eventendDate = this.customDateFormat(this.eventData.endDate, this.eventData.endTime)
+      const isToday = this.compareDate(eventDate, eventendDate, this.eventData)
+      if (isToday) {
+        this.currentEvent = true
+      }
+    })
+  }
+
+  customDateFormat(date: any, time: any) {
+    const stime = time.split('+')[0]
+    const hour = stime.substr(0, 2)
+    const min = stime.substr(2, 3)
+    return `${date} ${hour}${min}`
+  }
+
+  compareDate(selectedStartDate: any, selectedEndDate: any, eventData: any) {
+    const now = new Date()
+    const today = moment(now).format('YYYY-MM-DD HH:mm')
+
+    const day = new Date().getDate()
+    const year = new Date().getFullYear()
+    // tslint:disable-next-line:prefer-template
+    const month = ('0' + (now.getMonth() + 1)).slice(-2)
+    const todaysdate = `${year}-${month}-${day}`
+
+    const stime = eventData.startTime.split('+')[0]
+    const shour = stime.substr(0, 2) * 60
+    const smin = stime.substr(3, 2) * 1
+    const starttime = shour + smin
+
+    const currentTime = new Date().getHours() * 60 + new Date().getMinutes()
+    const minustime = starttime - currentTime
+    if (eventData.startDate === todaysdate && minustime < 16 && (selectedStartDate > today || selectedEndDate < today))  {
+      return true
+    }
+    return false
   }
 
   // fetchSingleCategoryDetails(cid: number) {
