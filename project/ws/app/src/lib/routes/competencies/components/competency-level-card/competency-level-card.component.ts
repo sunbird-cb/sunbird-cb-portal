6402a249b58a52b39f5617891655cd90271736a9
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-09-08 19:35:38 +0530

    Changes for detailed view

---
+++ project/ws/app/src/lib/routes/competencies/components/competency-level-card/competency-level-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core'
+import { NSCompetencie } from '../../models/competencies.model';
+
+@Component({
+  selector: 'ws-app-competency-level-card',
+  templateUrl: './competency-level-card.component.html',
+  styleUrls: ['./competency-level-card.component.scss']
+})
+export class CompetencyLevelCardComponent implements OnInit {
+  @Input()
+  data!: NSCompetencie.ICompetencie
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- project/ws/app/src/lib/routes/competencies/routes/competency-detailed-view/competency-detailed-view.component.ts
+++ project/ws/app/src/lib/routes/competencies/routes/competency-detailed-view/competency-detailed-view.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
+import { CompetenceService } from '../../services/competence.service'
 // tslint:disable
 import _ from 'lodash'
 import { ActivatedRoute } from '@angular/router'
 import { Subscription } from 'rxjs'
+import { NSCompetencie } from '../../models/competencies.model';
 
 @Component({
   selector: 'ws-app-competency-detailed-view',
@@ -13,13 +15,30 @@ export class CompetencyDetailedViewComponent implements OnInit, OnDestroy {
 
   private paramSubscription: Subscription | null = null
   competencyName: any = null
-  constructor(private activatedRoute: ActivatedRoute) { }
+  type: any = 'COMPETENCY'
+  competencyId: any = null
+  competencyData: any = null
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private competencySvc: CompetenceService,
+  ) { }
 
   ngOnInit() {
     this.paramSubscription = this.activatedRoute.params.subscribe(async params => {
+      this.competencyId = _.get(params, 'competencyId')
       this.competencyName = _.get(params, 'competencyName')
-      console.log("Name", this.competencyName)
+      // console.log("Name", this.competencyName)
     })
+
+
+    this.competencySvc
+      .fetchCompetencyDetails(this.competencyId, this.type)
+      .subscribe((reponse: NSCompetencie.ICompetencieResponse) => {
+        if (reponse.statusInfo && reponse.statusInfo.statusCode === 200) {
+          this.competencyData = reponse.responseData
+          console.log(this.competencyData)
+        }
+      })
   }
 
   ngOnDestroy() {
