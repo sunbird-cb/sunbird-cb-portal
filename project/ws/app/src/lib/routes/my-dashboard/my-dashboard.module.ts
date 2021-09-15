255a32cc50954e6fae857984f05f70b8c3e72f2e
SuvajitTarento <suvajit.chakraborty@tarento.com>
2021-09-15 17:42:10 +0530

    Changes for analytics

--- project/ws/app/src/lib/routes/my-dashboard/my-dashboard.module.ts
+++ project/ws/app/src/lib/routes/my-dashboard/my-dashboard.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatButtonModule, MatCardModule, MatIconModule, MatToolbarModule } from '@angular/material'
-import { BtnPageBackModule } from '@sunbird-cb/collection'
+import { BreadcrumbsOrgModule } from '@sunbird-cb/collection'
 import { MyDashboardHomeComponent } from './components/my-dashboard-home/my-dashboard-home.component'
 import { MyDashboardRoutingModule } from './my-dashboard-routing.module'
+import { RainDashboardsModule } from '@sunbird-cb/rain-dashboards'
 
 @NgModule({
   declarations: [MyDashboardHomeComponent],
@@ -14,7 +15,8 @@ import { MyDashboardRoutingModule } from './my-dashboard-routing.module'
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    BtnPageBackModule,
+    BreadcrumbsOrgModule,
+    RainDashboardsModule,
   ], exports: [MyDashboardHomeComponent],
 })
 export class MyDashboardModule { }
