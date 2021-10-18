321bab6b827d89574d4c1f052548a351b1c1a623
nitinraj-tarento <75484189+nitinraj-tarento@users.noreply.github.com>
2021-10-18 13:17:47 +0530

    feed back & report prob changes

--- project/ws/app/src/lib/routes/info/report-problem/components/reportproblem.component.ts
+++ project/ws/app/src/lib/routes/info/report-problem/components/reportproblem.component.ts
@@ -11,6 +11,12 @@ export class ReportproblemComponent implements OnInit {
   surveyId = 1632458489991
   // surveyTitle = 'Report a problem'
   // surveyId = 1625726181379
+  thankYouMessage = "Thank you for reporting your concern.";
+  thankYouDescription = "We are always looking forward to improvement.";
+  tyPrimaryBtnLink = "/page/home";
+  tySecondaryBtnLink = "/app/info/report-problem";
+  tyPrimaryBtnText = "Go to";
+  tySecondaryBtnText = "More feedback";
   apiData: object = {
     getAPI: `${this.domain}api/forms/getFormById?id=${this.surveyId}`,
     postAPI: `${this.domain}api/forms/saveFormSubmit`,
