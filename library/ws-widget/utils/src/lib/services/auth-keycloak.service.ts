cbc4fefb84af2c37677ec8f9dd7093cfe4ff241f
Amit Sengar <amitsengar@yahoo.com>
2022-05-10 13:27:43 +0530

    local session storage cleared and 419===> logout#7

--- library/ws-widget/utils/src/lib/services/auth-keycloak.service.ts
+++ library/ws-widget/utils/src/lib/services/auth-keycloak.service.ts
@@ -153,13 +153,13 @@ export class AuthKeycloakService {
     // alert(`${redirectUrl}apis/reset`)
     // window.location.href = `${redirectUrl}apis/reset`
     // window.location.href = 'http://localhost:3003/reset'
-    await this.http.get('/apis/reset').toPromise()
-    setTimeout(window.location.href = redirectUrl, 13000)
+    //  this.http.get('/apis/reset').toPromise()
+    setTimeout(window.location.href = `${redirectUrl}apis/reset`, 13000)
     // logoutRedirectUrl = redirectUrl
     // if (this.msAuthSvc.isLogoutRequired) {
     //   this.keycloakSvc.logout(this.msAuthSvc.logoutUrl(redirectUrl))
     // } else {
-    this.keycloakSvc.logout(redirectUrl)
+    // this.keycloakSvc.logout(redirectUrl)
     // }
   }
 
