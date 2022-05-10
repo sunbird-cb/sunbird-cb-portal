437acc9887f78448eeb0243630a8673cbac6e0a0
Amit Sengar <amitsengar@yahoo.com>
2022-05-10 13:28:12 +0530

    local session storage cleared and 419===> logout#8

--- library/ws-widget/utils/src/lib/services/auth-keycloak.service.ts
+++ library/ws-widget/utils/src/lib/services/auth-keycloak.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+// import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { KeycloakEvent, KeycloakEventType, KeycloakInitOptions, KeycloakService } from 'keycloak-angular'
 import { fromEvent, ReplaySubject } from 'rxjs'
@@ -23,7 +23,7 @@ const storageKey = 'kc'
 export class AuthKeycloakService {
   private loginChangeSubject = new ReplaySubject<boolean>(1)
   constructor(
-    private http: HttpClient,
+    // private http: HttpClient,
     private configSvc: ConfigurationsService,
     private keycloakSvc: KeycloakService,
     private msAuthSvc: AuthMicrosoftService,
