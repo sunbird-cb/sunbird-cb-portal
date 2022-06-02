3857f2f5c7d50737b4be2ace29ac6c550cc6a7ff
NandiniAV <nandini.achutha@tarento.com>
2022-06-02 18:22:59 +0530

    Current & Desired competency listing & selection api integration

--- project/ws/app/src/lib/routes/profile-v3/routes/current-competencies/current-competencies.component.ts
+++ project/ws/app/src/lib/routes/profile-v3/routes/current-competencies/current-competencies.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { ProfileV3Service } from '../../services/profile_v3.service'
+import { NSProfileDataV3 } from '../../models/profile-v3.models'
 
 @Component({
   selector: 'ws-app-current-competencies',
   templateUrl: './current-competencies.component.html',
-  styleUrls: ['./current-competencies.component.scss']
+  styleUrls: ['./current-competencies.component.scss'],
 })
 export class CurrentCompetenciesComponent implements OnInit {
+  searchJson!: NSProfileDataV3.ISearch[]
+  allCompetencies!: NSProfileDataV3.ICompetencie[]
 
-  constructor() { }
+  constructor(private competencySvc: ProfileV3Service) { }
 
   ngOnInit() {
+    this.getCompetencies()
+  }
+
+  getCompetencies() {
+    this.searchJson = [
+      { type: 'COMPETENCY', field: 'name', keyword: '' },
+      { type: 'COMPETENCY', field: 'status', keyword: 'VERIFIED' },
+    ]
+
+    const searchObj = {
+      searches: this.searchJson,
+      childNodes: true,
+    }
+    this.competencySvc
+      .getAllCompetencies(searchObj)
+      .subscribe((reponse: any) => {
+        if (reponse.statusInfo && reponse.statusInfo.statusCode === 200) {
+          this.allCompetencies = reponse.responseData
+        }
+      })
+  }
+
+  getSelectedCompetency(event: any) {
+    console.log('getSelectedCompetency ********', event)
+  }
+
+  getSelectedCompLevel(event: any) {
+    console.log('getSelectedCompetencyLEvel ==========', event)
   }
 
 }
--- project/ws/app/src/lib/routes/profile-v3/routes/desired-competencies/desired-competencies.component.ts
+++ project/ws/app/src/lib/routes/profile-v3/routes/desired-competencies/desired-competencies.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ProfileV3Service } from '../../services/profile_v3.service'
+import { NSProfileDataV3 } from '../../models/profile-v3.models'
 
 @Component({
   selector: 'ws-app-desired-competencies',
   templateUrl: './desired-competencies.component.html',
-  styleUrls: ['./desired-competencies.component.scss']
+  styleUrls: ['./desired-competencies.component.scss'],
 })
 export class DesiredCompetenciesComponent implements OnInit {
+  searchJson!: NSProfileDataV3.ISearch[]
+  allCompetencies!: NSProfileDataV3.ICompetencie[]
 
-  constructor() { }
+  constructor(private competencySvc: ProfileV3Service) { }
 
   ngOnInit() {
+    this.getCompetencies()
+  }
+
+  getCompetencies() {
+    this.searchJson = [
+      { type: 'COMPETENCY', field: 'name', keyword: '' },
+      { type: 'COMPETENCY', field: 'status', keyword: 'VERIFIED' },
+    ]
+
+    const searchObj = {
+      searches: this.searchJson,
+      childNodes: true,
+    }
+    this.competencySvc
+      .getAllCompetencies(searchObj)
+      .subscribe((reponse: any) => {
+        if (reponse.statusInfo && reponse.statusInfo.statusCode === 200) {
+          this.allCompetencies = reponse.responseData
+        }
+      })
+  }
+
+  getSelectedCompetency(event: any) {
+    console.log('getSelectedCompetency ********', event)
+  }
+
+  getSelectedCompLevel(event: any) {
+    console.log('getSelectedCompetencyLEvel ==========', event)
   }
 
 }
