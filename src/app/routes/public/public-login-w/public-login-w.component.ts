c0a923f72f8995b922955938689165e460e34737
Amit Sengar <aAmitSengar@users.noreply.github.com>
2022-07-21 14:46:06 +0530

    Assessment player (#512)
    * time fix
    
    * New API structure To play a Practice Assessment Updated
    
    * New API structure To play a Practice Assessment Updated #1
    
    * player Updates
    
    * parichay added to public page
    
    * player update
    
    * parichay API call
    
    * parichay API call#1
    
    * Role and Activity BugFix
    
    * Login Fix #1
    
    * Login Parichay  Fix #1

--- src/app/routes/public/public-login-w/public-login-w.component.ts
+++ src/app/routes/public/public-login-w/public-login-w.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
 // import { ConfigurationsService, NsPage } from '@sunbird-cb/utils'
 import { Subscription } from 'rxjs'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Params } from '@angular/router'
 import { HttpClient } from '@angular/common/http'
+// tslint:disable-next-line
+import _ from 'lodash'
 
 @Component({
     selector: 'public-login-w',
@@ -21,14 +23,22 @@ export class PublicLoginWComponent implements OnInit, OnDestroy {
     ) { }
 
     ngOnInit() {
-        this.subscriptionContact = this.activateRoute.queryParamMap.subscribe(data => {
-            this.data = data
+        this.subscriptionContact = this.activateRoute.queryParamMap.subscribe((response: Params) => {
+            this.data = _.get(response, 'params')
+            const code = _.get(this.data, 'code')
+            // const authuser = _.get(this.data, 'authuser')
+            // const hd = _.get(this.data, 'hd')
+            // const prompt = _.get(this.data, 'prompt')
+            // const scope = _.get(this.data, 'scope')
+            const state = _.get(this.data, 'state')
             // tslint:disable-next-line
-            console.log(data)
-            this.httpClient.get(`/apis/public/v8/google/callback?${data}`).subscribe(rData => {
-                // tslint:disable-next-line
-                console.log(rData)
-            })
+            // console.log(`/apis/public/v8/google/callback?code=${code}&scope=${scope}&authuser=${authuser}&hd=${hd}&prompt=${prompt}`)
+            if (code) {
+                this.httpClient.get(`/apis/public/v8/parichay/callback`, { params: { code: code, state: state } }).subscribe(rData => {
+                    // tslint:disable-next-line
+                    console.log(rData)
+                })
+            }
         })
     }
     ngOnDestroy() {
