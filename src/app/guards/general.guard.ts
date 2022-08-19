36d0d2534e66109126e2c7b091482c2590b31459
nitin raj <nitin.raj@tarento.com>
2022-08-19 11:12:27 +0530

    public home page ui changes

--- src/app/guards/general.guard.ts
+++ src/app/guards/general.guard.ts
@@ -41,7 +41,7 @@ export class GeneralGuard implements CanActivate {
     return returnValue
   }
   private async shouldAllow<T>(
-    state: RouterStateSnapshot,
+    // state: RouterStateSnapshot,
     requiredFeatures: string[],
     requiredRoles: string[],
   ): Promise<T | UrlTree | boolean> {
