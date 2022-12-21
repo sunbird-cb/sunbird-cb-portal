900a7c7839f0695c481900352deffff1f04337f8
Amit Sengar <amitsengar@yahoo.com>
2022-12-21 17:44:21 +0530

    recomended identifier to id

--- project/ws/app/src/lib/routes/network-v2/routes/network-recommended/network-recommended.component.ts
+++ project/ws/app/src/lib/routes/network-v2/routes/network-recommended/network-recommended.component.ts
@@ -50,7 +50,7 @@ export class NetworkRecommendedComponent implements OnInit {
     const fulldata = this.data
     this.data = []
     fulldata.forEach((user: any) => {
-      this.networkV2Service.fetchProfile(user.identifier).subscribe((res: any) => {
+      this.networkV2Service.fetchProfile(user.id).subscribe((res: any) => {
         // this.data.push(res.result.UserProfile[0])
         this.data.push(res.result.response)
       })
