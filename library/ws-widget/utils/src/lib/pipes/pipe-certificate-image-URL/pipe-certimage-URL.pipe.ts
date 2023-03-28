8f81fa8b32b48064be15aa5ea1251ba7025d6c12
nitinraj-tarento <nitin.raj@tarento.com>
2023-03-28 19:05:03 +0530

    cert img fix

--- library/ws-widget/utils/src/lib/pipes/pipe-certificate-image-URL/pipe-certimage-URL.pipe.ts
+++ library/ws-widget/utils/src/lib/pipes/pipe-certificate-image-URL/pipe-certimage-URL.pipe.ts
@@ -21,26 +21,45 @@ export class PipeCertificateImageURL implements PipeTransform {
         const finalURL = `${environment.contentHost}/${environment.contentBucket}/content${mainUrl}`
         return value ? finalURL : ''
       }
-      if (value.indexOf('/igot/collection') > -1) {
-        const mainUrl = value && value.split('/igot').pop() || ''
-        const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
-        return value ? finalURL : ''
-      }
-      if (value.indexOf('/igot/collection') === -1) {
-        const mainUrl = value && value.split('/igot').pop() || ''
-        const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
-        return value ? finalURL : ''
-      }
       if (value.indexOf('/igotprod/collection') > -1) {
         const mainUrl = value && value.split('/igotprod').pop() || ''
-        const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        // const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        // const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat(${mainUrl})
+        const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat(`${mainUrl}`)
         return value ? finalURL : ''
       }
       if (value.indexOf('/igotprod/collection') === -1) {
         const mainUrl = value && value.split('/igotprod').pop() || ''
-        const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        // const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat(`${mainUrl}`)
         return value ? finalURL : ''
       }
+      if (value.indexOf('/igotbm/collection') > -1) {
+        const mainUrl = value && value.split('/igotbm').pop() || ''
+        // const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        // const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat(${mainUrl})
+        const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat(`${mainUrl}`)
+        return value ? finalURL : ''
+      }
+      if (value.indexOf('/igotbm/collection') === -1) {
+        const mainUrl = value && value.split('/igotbm').pop() || ''
+        // const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat(`${mainUrl}`)
+        return value ? finalURL : ''
+      }
+      if (value.indexOf('/igot/collection') > -1) {
+        const mainUrl = value && value.split('/igot/').pop() || ''
+        // const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat("/").concat(`${mainUrl}`)
+        return value ? finalURL : ''
+      }
+      if (value.indexOf('/igot/collection') === -1) {
+        const mainUrl = value && value.split('/igot/').pop() || ''
+        // const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
+        const finalURL = `${environment.contentHost}`.concat("/").concat(`${environment.contentBucket}`).concat("/").concat(`${mainUrl}`)
+        return value ? finalURL : ''
+      }
+   
       if (value.indexOf('/content/collection') > -1) {
         const mainUrl = value && value.split('/content').pop() || ''
         const finalURL = `${environment.contentHost}/${environment.contentBucket}${mainUrl}`
