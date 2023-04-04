6ac072c61047c1c22d515e25c02ecd8b055b0f22
Amit Sengar <amitsengar@yahoo.com>
2023-04-04 14:35:41 +0530

    public API updated for Positions

--- src/app/routes/public/public-signup/signup.service.ts
+++ src/app/routes/public/public-signup/signup.service.ts
@@ -8,10 +8,10 @@ const API_END_POINTS = {
   GET_ALL_STATES: '/apis/public/v8/org/v1/list',
   GET_DEPARTMENTS_OF_STATE: '/apis/public/v8/org/v1/list',
   GET_ORGS_OF_DEPT: '/apis/public/v8/org/v1/list',
-  GET_POSITIONS: '/apis/proxies/v8/user/v1/positions',
   sendOtp: '/api/otp/v1/generate',
   ReSendOtp: '/api/otp/v1/generate',
   VerifyOtp: '/api/otp/v1/verify',
+  GET_POSITIONS: '/api/user/v1/positions',
 }
 
 @Injectable({
