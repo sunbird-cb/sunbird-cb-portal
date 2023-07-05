d54e630aa41d86fbd20b63ba8baa4a390fa0eca1
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2023-07-05 17:03:25 +0530

    added html-mobile component

---
+++ project/ws/viewer/src/lib/routes/html-mobile/html-mobile.component.ts
@@ -0,0 +1,436 @@
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { AccessControlService } from '@ws/author'
+import { NsContent, NsDiscussionForum, WidgetContentService } from '@sunbird-cb/collection'
+import { NsWidgetResolver } from '@sunbird-cb/resolver'
+import {
+  EventService,
+  SubapplicationRespondService,
+  WsEvents,
+  ConfigurationsService,
+} from '@sunbird-cb/utils'
+import { fromEvent, Subscription } from 'rxjs'
+import { filter } from 'rxjs/operators'
+import { ViewerUtilService } from '../../viewer-util.service'
+import { environment } from 'src/environments/environment'
+@Component({
+  selector: 'viewer-html-mobile',
+  templateUrl: './html-mobile.component.html',
+  styleUrls: ['./html-mobile.component.scss'],
+})
+export class HtmlMobileComponent implements OnInit, OnDestroy {
+  
+  private routeDataSubscription: Subscription | null = null
+  private responseSubscription: Subscription | null = null
+  private viewerDataSubscription: Subscription | null = null
+  forPreview = window.location.href.includes('/public/') || window.location.href.includes('&preview=true')
+  isNotEmbed = true
+  isFetchingDataComplete = false
+  htmlData: NsContent.IContent | null = null
+  oldData: NsContent.IContent | null = null
+  alreadyRaised = false
+  subApp = false
+  discussionForumWidget: NsWidgetResolver.IRenderConfigWithTypedData<
+    NsDiscussionForum.IDiscussionForumInput
+  > | null = null
+  uuid: string | null | undefined = null
+  realTimeProgressRequest = {
+    content_type: 'Resource',
+    primaryCategory: NsContent.EPrimaryCategory.RESOURCE,
+    current: ['0'],
+    max_size: 0,
+    mime_type: NsContent.EMimeTypes.ZIP,
+    user_id_type: 'uuid',
+  }
+  realTimeProgressTimer: any
+  hasFiredRealTimeProgress = false
+  isPreviewMode = false
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private contentSvc: WidgetContentService,
+    private viewerSvc: ViewerUtilService,
+    private respondSvc: SubapplicationRespondService,
+    private eventSvc: EventService,
+    private accessControlSvc: AccessControlService,
+    private configSvc: ConfigurationsService
+  ) { }
+
+  ngOnInit() {
+    // this.activatedRoute.data.subscribe(data => {
+    //   this.uuid = data.profileData.data.userId
+    // })
+    this.uuid = this.configSvc.userProfile ? this.configSvc.userProfile.userId : ''
+    this.isNotEmbed = !(
+      window.location.href.includes('/embed/') ||
+      this.activatedRoute.snapshot.queryParams.embed === 'true'
+    )
+    debugger
+    if (
+      this.activatedRoute.snapshot.queryParamMap.get('preview') === 'true' &&
+      !this.accessControlSvc.authoringConfig.newDesign
+    ) {
+      this.isPreviewMode = true
+      // to do make sure the data updates for two consecutive resource of same mimeType
+      // this.viewerDataSubscription = this.viewerSvc
+      //   .getContent(this.activatedRoute.snapshot.paramMap.get('resourceId') || '')
+      this.viewerDataSubscription = this.activatedRoute.data.subscribe(
+        async data => {
+          data.content.data.artifactUrl =
+            data.content.data.artifactUrl.indexOf('ScormCoursePlayer') > -1
+              ? `${data.content.data.artifactUrl.replace(/%20/g, '')}&Param1=${this.uuid}`
+              : data.content.data.artifactUrl.replace(/%20/g, '')
+          const tempHtmlData = data.content.data
+          if (tempHtmlData) {
+            data.artifactUrl = (data.content.data.artifactUrl.startsWith('https://')
+              ? data.content.data.artifactUrl
+              : data.content.data.artifactUrl.startsWith('http://')
+                ? data.content.data.artifactUrl
+                : `https://${data.content.data.artifactUrl}`).replace(/ /ig, '').replace(/%20/ig, '').replace(/\n/ig, '')
+            this.htmlData = tempHtmlData
+            if (!this.alreadyRaised && this.oldData) {
+              // this.raiseEvent(WsEvents.EnumTelemetrySubType.Unloaded, this.oldData)      //mobile requirement telemetry disabled
+              if (!this.hasFiredRealTimeProgress) {
+                // this.fireRealTimeProgress()
+                if (this.realTimeProgressTimer) {
+                  clearTimeout(this.realTimeProgressTimer)
+                }
+              }
+              this.subApp = false
+            }
+            this.raiseRealTimeProgress()
+            if (this.htmlData) {
+              this.oldData = this.htmlData
+              this.alreadyRaised = true
+              // this.raiseEvent(WsEvents.EnumTelemetrySubType.Loaded, this.htmlData)
+              this.responseSubscription = await fromEvent<MessageEvent>(window, 'message')
+                .pipe(
+                  filter(
+                    (event: MessageEvent) =>
+                      Boolean(event) &&
+                      Boolean(event.data) &&
+                      Boolean(event.source && typeof event.source.postMessage === 'function'),
+                  ),
+                )
+                .subscribe(async (event: MessageEvent) => {
+                  const contentWindow = event.source as Window
+                  if (event.data.requestId && this.htmlData) {
+                    switch (event.data.requestId) {
+                      case 'LOADED':
+                        await this.respondSvc.loadedRespond(
+                          contentWindow,
+                          event.data.subApplicationName,
+                          this.htmlData.identifier,
+                        )
+                        if (event.data.subApplicationName === 'RBCP') {
+                          this.subApp = true
+                        }
+                        break
+                      case 'CONTINUE_LEARNING':
+                        await this.respondSvc.continueLearningRespond(
+                          this.htmlData.identifier,
+                          event.data.data.continueLearning,
+                        )
+                        break
+                      case 'TELEMETRY':
+                        await this.respondSvc.telemetryEvents(event.data)
+                        break
+                      default:
+                        break
+                    }
+                  }
+                })
+            }
+            // if (this.accessControlSvc.hasAccess(data as any, true)) {
+            //   if (data && data.artifactUrl.indexOf('content-store') >= 0) {
+            //     // await this.setS3Cookie(data.identifier)
+            //     this.htmlData = data
+            //   } else {
+            //     this.htmlData = data
+            //   }
+
+          }
+          if (this.htmlData) {
+            this.formDiscussionForumWidget(this.htmlData)
+            if (this.discussionForumWidget) {
+              this.discussionForumWidget.widgetData.isDisabled = true
+            }
+          }
+        })
+    } else {
+      this.routeDataSubscription = this.activatedRoute.data.subscribe(
+        async data => {
+          // data.content.data.artifactUrl =
+          //   data.content.data.artifactUrl.startsWith('/scorm-player') ?
+          //     `/apis/proxies/v8${data.content.data.artifactUrl}` : data.content.data.artifactUrl
+          data.content.data.artifactUrl =
+            data.content.data.artifactUrl.indexOf('ScormCoursePlayer') > -1
+              ? `${data.content.data.artifactUrl.replace(/%20/g, '')}&Param1=${this.uuid}`
+              : data.content.data.artifactUrl.replace(/%20/g, '')
+          const tempHtmlData = data.content.data
+          if (this.alreadyRaised && tempHtmlData) {
+            // this.raiseEvent(WsEvents.EnumTelemetrySubType.Unloaded, tempHtmlData) //mobile requirement telemetry disabled
+            if (!this.hasFiredRealTimeProgress) {
+              // this.fireRealTimeProgress()
+              if (this.realTimeProgressTimer) {
+                clearTimeout(this.realTimeProgressTimer)
+              }
+            }
+            this.subApp = false
+          }
+          if (tempHtmlData) {
+            this.formDiscussionForumWidget(tempHtmlData)
+          }
+          if (tempHtmlData && tempHtmlData.artifactUrl.indexOf('content-store') >= 0) {
+            await this.setS3Cookie(tempHtmlData.identifier)
+            this.htmlData = tempHtmlData
+          } else {
+            this.htmlData = tempHtmlData
+          }
+          this.raiseRealTimeProgress()
+          if (this.htmlData) {
+            this.oldData = this.htmlData
+            this.alreadyRaised = true
+            // this.raiseEvent(WsEvents.EnumTelemetrySubType.Loaded, this.htmlData)        //mobile requirement telemetry disabled
+            this.responseSubscription = await fromEvent<MessageEvent>(window, 'message')
+              .pipe(
+                filter(
+                  (event: MessageEvent) =>
+                    Boolean(event) &&
+                    Boolean(event.data) &&
+                    Boolean(event.source && typeof event.source.postMessage === 'function'),
+                ),
+              )
+              .subscribe(async (event: MessageEvent) => {
+                const contentWindow = event.source as Window
+                if (event.data.requestId && this.htmlData) {
+                  switch (event.data.requestId) {
+                    case 'LOADED':
+                      await this.respondSvc.loadedRespond(
+                        contentWindow,
+                        event.data.subApplicationName,
+                        this.htmlData.identifier,
+                      )
+                      if (event.data.subApplicationName === 'RBCP') {
+                        this.subApp = true
+                      }
+                      break
+                    case 'CONTINUE_LEARNING':
+                      await this.respondSvc.continueLearningRespond(
+                        this.htmlData.identifier,
+                        event.data.data.continueLearning,
+                      )
+                      break
+                    case 'TELEMETRY':
+                      await this.respondSvc.telemetryEvents(event.data)
+                      break
+                    default:
+                      break
+                  }
+                }
+              })
+          }
+          this.isFetchingDataComplete = true
+        },
+        () => { },
+      )
+    }
+  }
+
+  async saveContinueLearning(content: NsContent.IContent | null) {
+    return new Promise(async resolve => {
+      if (this.activatedRoute.snapshot.queryParams.collectionType &&
+        content &&
+        this.activatedRoute.snapshot.queryParams.collectionType.toLowerCase() === 'playlist') {
+        const reqBody = {
+          contextPathId: this.activatedRoute.snapshot.queryParams.collectionId
+            ? this.activatedRoute.snapshot.queryParams.collectionId
+            : content
+              ? content.identifier
+              : '',
+          resourceId: content.identifier,
+          data: JSON.stringify({
+            timestamp: Date.now(),
+            contextFullPath: [this.activatedRoute.snapshot.queryParams.collectionId, content.identifier],
+          }),
+          dateAccessed: Date.now(),
+          contextType: 'playlist',
+        }
+        this.contentSvc.saveContinueLearning(reqBody).toPromise().catch().finally(() => {
+          resolve(true)
+        }
+        )
+      } else {
+        const reqBody = {
+          contextPathId: this.activatedRoute.snapshot.queryParams.collectionId
+            ? this.activatedRoute.snapshot.queryParams.collectionId
+            : content
+              ? content.identifier
+              : '',
+          resourceId: content ? content.identifier : '',
+          data: JSON.stringify({ timestamp: Date.now() }),
+          dateAccessed: Date.now(),
+        }
+        this.contentSvc.saveContinueLearning(reqBody).toPromise().catch().finally(() => {
+          resolve(true)
+        }
+        )
+      }
+    })
+  }
+
+  generateUrl(oldUrl: string) {
+    const chunk = oldUrl.split('/')
+    const newChunk = environment.azureHost.split('/')
+    const newLink = []
+    for (let i = 0; i < chunk.length; i += 1) {
+      if (i === 2) {
+        newLink.push(newChunk[i])
+      } else if (i === 3) {
+        newLink.push(environment.azureBucket)
+      } else {
+        newLink.push(chunk[i])
+      }
+    }
+    const newUrl = newLink.join('/')
+    return newUrl
+  }
+
+  async ngOnDestroy() {
+    if (this.htmlData) {
+      if (!this.subApp || this.activatedRoute.snapshot.queryParams.collectionId) {
+        await this.saveContinueLearning(this.htmlData)
+      }
+    }
+    // if (this.htmlData) {
+    //   this.raiseEvent(WsEvents.EnumTelemetrySubType.Unloaded, this.htmlData)      //mobile requirement telemetry disabled
+    // }
+    if (this.routeDataSubscription) {
+      this.routeDataSubscription.unsubscribe()
+    }
+    if (this.responseSubscription) {
+      this.respondSvc.unsubscribeResponse()
+      this.responseSubscription.unsubscribe()
+    }
+    if (this.viewerDataSubscription) {
+      this.viewerDataSubscription.unsubscribe()
+    }
+    // if (!this.hasFiredRealTimeProgress && !this.forPreview) {
+    //   this.fireRealTimeProgress()
+    //   if (this.realTimeProgressTimer) {
+    //     clearTimeout(this.realTimeProgressTimer)
+    //   }
+    // }
+  }
+
+  formDiscussionForumWidget(content: NsContent.IContent) {
+    this.discussionForumWidget = {
+      widgetData: {
+        description: content.description,
+        id: content.identifier,
+        name: NsDiscussionForum.EDiscussionType.LEARNING,
+        title: content.name,
+        initialPostCount: 2,
+        isDisabled: this.forPreview,
+      },
+      widgetSubType: 'discussionForum',
+      widgetType: 'discussionForum',
+    }
+  }
+
+  private async setS3Cookie(contentId: string) {
+    await this.contentSvc
+      .setS3Cookie(contentId)
+      .toPromise()
+      .catch(() => {
+        // throw new DataResponseError('COOKIE_SET_FAILURE')
+      })
+    return
+  }
+
+  raiseEvent(state: WsEvents.EnumTelemetrySubType, data: NsContent.IContent) {
+    // if (this.forPreview) {
+    //   return
+    // }
+    const event = {
+      eventType: WsEvents.WsEventType.Telemetry,
+      eventLogLevel: WsEvents.WsEventLogLevel.Info,
+      from: 'html',
+      to: '',
+      data: {
+        state,
+        type: WsEvents.WsTimeSpentType.Player,
+        mode: WsEvents.WsTimeSpentMode.Play,
+        courseId: null,
+        content: data,
+        identifier: data ? data.identifier : null,
+        isCompleted: true,
+        mimeType: NsContent.EMimeTypes.HTML,
+        isIdeal: false,
+        url: data ? data.artifactUrl : null,
+        object: { id: data ? data.identifier : null, type: data ? data.primaryCategory : '' },
+      },
+    }
+    this.eventSvc.dispatchEvent(event)
+  }
+
+  private raiseRealTimeProgress() {
+    this.realTimeProgressRequest = {
+      ...this.realTimeProgressRequest,
+      current: ['1'],
+      max_size: 1,
+    }
+    if (this.realTimeProgressTimer) {
+      clearTimeout(this.realTimeProgressTimer)
+    }
+    this.hasFiredRealTimeProgress = false
+    this.realTimeProgressTimer = setTimeout(() => {
+      this.hasFiredRealTimeProgress = true
+      this.fireRealTimeProgress()
+      // tslint:disable-next-line: align
+    }, 6 * 1000)
+  }
+
+  private fireRealTimeProgress() {
+    if (this.htmlData) {
+      if (
+        this.htmlData.primaryCategory === NsContent.EPrimaryCategory.COURSE &&
+        this.htmlData.isExternal
+      ) {
+        return
+      }
+      if (
+        this.htmlData.resourceType &&
+        this.htmlData.resourceType.toLowerCase() === 'certification'
+      ) {
+        return
+      }
+    }
+    if ((this.htmlData || ({} as any)).isIframeSupported &&
+      (this.htmlData || ({} as any)).isIframeSupported.toLowerCase() !== 'yes') {
+      return
+    }
+    if (this.htmlData) {
+      if (this.htmlData.sourceName === 'Cross Knowledge') {
+        return
+      }
+    }
+    if (this.htmlData) {
+      this.realTimeProgressRequest.content_type = this.htmlData.contentType
+      this.realTimeProgressRequest.primaryCategory = this.htmlData.primaryCategory
+
+      const collectionId = this.activatedRoute.snapshot.queryParams.collectionId ?
+        this.activatedRoute.snapshot.queryParams.collectionId : ''
+
+      const batchId = this.activatedRoute.snapshot.queryParams.batchId ?
+        this.activatedRoute.snapshot.queryParams.batchId : ''
+
+      this.viewerSvc.realTimeProgressUpdate(
+        this.htmlData.identifier,
+        this.realTimeProgressRequest,
+        collectionId,
+        batchId
+      )
+    }
+    return
+  }
+}
