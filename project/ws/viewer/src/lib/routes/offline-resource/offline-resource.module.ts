cbdb0bc1f28b09dee6227e1788daedc18ce21541
christyfernandes <christyabcd.05@gmail.com>
2023-07-18 16:36:00 +0530

    new player route for offline resource

--- project/ws/viewer/src/lib/routes/offline-resource/offline-resource.module.ts
+++ project/ws/viewer/src/lib/routes/offline-resource/offline-resource.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { OfflineResourceComponent } from './offline-resource.component'
+import { RouterModule, Routes } from '@angular/router'
+import { ViewerResolve } from '../../viewer.resolve'
 
-
+const routes: Routes = [
+  {
+    path: ':resourceId',
+    component: OfflineResourceComponent,
+    resolve: {
+      content: ViewerResolve,
+    },
+  },
+]
 
 @NgModule({
   declarations: [OfflineResourceComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [ViewerResolve],
 })
 export class OfflineResourceModule { }
