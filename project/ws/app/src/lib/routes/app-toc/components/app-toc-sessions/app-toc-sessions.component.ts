dc0ceae06d623aaa3753aea4aa19c67b23b255e1
Christyfernandes <christyabcd.05@gmail.com>
2023-08-18 20:52:27 +0530

    Task #95921:  added session tab component

---
+++ project/ws/app/src/lib/routes/app-toc/components/app-toc-sessions/app-toc-sessions.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { NsContent } from '@sunbird-cb/collection'
+
+@Component({
+  selector: 'ws-app-app-toc-sessions',
+  templateUrl: './app-toc-sessions.component.html',
+  styleUrls: ['./app-toc-sessions.component.scss']
+})
+export class AppTocSessionsComponent implements OnInit {
+  @Input() batchId!: string
+  @Input() content: NsContent.IContent | null = null
+  @Input() forPreview = false
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
