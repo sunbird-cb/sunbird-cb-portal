81c49fa000377e918713adc1f02da66ff4ed9e9b
Venky <venky.kandagaddala@gmail.com>
2023-09-07 19:15:51 +0530

    Chatbot UI

--- src/app/component/app-chatbot-issue/app-chatbot-issue.component.ts
+++ src/app/component/app-chatbot-issue/app-chatbot-issue.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppChatbotIssueComponent implements OnInit {
 
+  level1Questions: any [] = [
+    'What is Mission Karmayogi?',
+    'What is iGOT?', 'How to register?',
+    'I am unable to login?',
+    'Why I am unable to get OTP?',
+    'Why cannot I find my MDO?']
+    showLevel1ItemCount = 3;
+    showLevel2ItemCount = 3;
+    clickedL1ButtonIndex: number | null = null
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getQuestion(count: number){
+    return this.level1Questions.slice(0, count)
+  }
+
+  onButtonClick(index: number) {
+    this.clickedL1ButtonIndex = index;
   }
 
 }
