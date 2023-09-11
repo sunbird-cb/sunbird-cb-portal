4d0d50191b17309faccd4739272d9ae4a6c6992a
nitinraj-tarento <nitin.raj@tarento.com>
2023-09-11 13:39:34 +0530

    zoho links removed

--- src/app/routes/public/public-contacthome/public-contacthome.component.ts
+++ src/app/routes/public/public-contacthome/public-contacthome.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, AfterViewInit, ElementRef, Inject } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ConfigurationsService, NsPage } from '@sunbird-cb/utils'
 import { environment } from 'src/environments/environment'
-import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'ws-public-contacthome',
   templateUrl: './public-contacthome.component.html',
   styleUrls: ['./public-contacthome.component.scss'],
 })
-export class PublicContacthomeComponent implements OnInit, AfterViewInit {
+export class PublicContacthomeComponent implements OnInit {
   contactUsMail = ''
   environment!: any
   meetLink = ''
@@ -18,9 +17,10 @@ export class PublicContacthomeComponent implements OnInit, AfterViewInit {
   pageNavbar: Partial<NsPage.INavBackground> = this.configSvc.pageNavBar
 
   constructor(
-    private configSvc: ConfigurationsService,
-    @Inject(DOCUMENT) private document: Document, 
-    private elementRef:ElementRef) {}
+    private configSvc: ConfigurationsService
+    // @Inject(DOCUMENT) private document: Document, 
+    // private elementRef:ElementRef
+    ) {}
 
   ngOnInit() {
     this.environment = environment
@@ -36,17 +36,17 @@ export class PublicContacthomeComponent implements OnInit, AfterViewInit {
     event.preventDefault()
   }
 
-  ngAfterViewInit() {
-    var s = this.document.createElement("script")
-    s.type = "text/javascript"
-    s.src = "https://desk.zoho.in/portal/api/web/inapp/120349000000222059?orgId=60023043070"
-    s.nonce = "{place_your_nonce_value_here}"
-    this.elementRef.nativeElement.appendChild(s)
+  // ngAfterViewInit() {
+  //   var s = this.document.createElement("script")
+  //   s.type = "text/javascript"
+  //   s.src = "https://desk.zoho.in/portal/api/web/inapp/120349000000222059?orgId=60023043070"
+  //   s.nonce = "{place_your_nonce_value_here}"
+  //   this.elementRef.nativeElement.appendChild(s)
 
-    var ss = this.document.createElement("script")
-    ss.type = "text/javascript"
-    ss.src = "https://desk.zoho.in/portal/api/feedbackwidget/120349000000212259?orgId=60023043070&displayType=popout"
-    this.elementRef.nativeElement.appendChild(ss)
-  }
+  //   var ss = this.document.createElement("script")
+  //   ss.type = "text/javascript"
+  //   ss.src = "https://desk.zoho.in/portal/api/feedbackwidget/120349000000212259?orgId=60023043070&displayType=popout"
+  //   this.elementRef.nativeElement.appendChild(ss)
+  // }
 
 }
