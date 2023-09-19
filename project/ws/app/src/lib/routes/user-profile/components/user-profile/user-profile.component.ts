424fcb2be776ec1484ddd096093ec605ca7c0336
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2023-09-19 16:28:58 +0530

    lint errors fix

--- project/ws/app/src/lib/routes/user-profile/components/user-profile/user-profile.component.ts
+++ project/ws/app/src/lib/routes/user-profile/components/user-profile/user-profile.component.ts
@@ -1281,7 +1281,7 @@ export class UserProfileComponent implements OnInit, OnDestroy {
           if (item === name) {
 
             switch (name) {
-              
+
               case 'knownLanguages': return personalDetail['knownLanguages'] = form.value.knownLanguages
               case 'dob': return personalDetail['dob'] = form.value.dob
               case 'nationality': return personalDetail['nationality']  = form.value.nationality
