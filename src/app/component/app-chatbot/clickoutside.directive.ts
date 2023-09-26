b1d922a67c5e7ac7fd885a28ed250301a6a13f05
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2023-09-26 17:27:22 +0530

    changes in the app-chatbot

---
+++ src/app/component/app-chatbot/clickoutside.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, Output, EventEmitter, HostListener,OnInit } from "@angular/core";
+import { fromEvent } from "rxjs";
+import { take } from "rxjs/operators";
+
+@Directive({
+  selector: "[clickOutside]"
+})
+export class ClickOutsideDirective implements OnInit {
+  @Output() clickOutside = new EventEmitter();
+
+  captured = false;
+
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener("document:click", ["$event.target"])
+  onClick(target: any) {
+    if (!this.captured) {
+      return;
+    }
+
+    if (!this.elRef.nativeElement.contains(target)) {
+      this.clickOutside.emit();
+    }
+  }
+
+  ngOnInit() {
+    fromEvent(document, "click", { capture: true })
+      .pipe(take(1))
+      .subscribe(() => (this.captured = true));
+  }
+}
