9db8fb76fce356883b66b9b38f1c50fb60b4fa4e
nitinraj-tarento <nitin.raj@tarento.com>
2023-09-29 13:55:24 +0530

    fetch user batch endpoint and function updated for profile page

--- library/ws-widget/collection/src/lib/_services/widget-user.service.ts
+++ library/ws-widget/collection/src/lib/_services/widget-user.service.ts
@@ -11,6 +11,9 @@ const API_END_POINTS = {
     `${PROTECTED_SLAG_V8}/user/group/fetchUserGroup?userId=${userId}`,
   FETCH_USER_ENROLLMENT_LIST: (userId: string | undefined) =>
     // tslint:disable-next-line: max-line-length
+    `/apis/proxies/v8/learner/course/v1/user/enrollment/list/${userId}?orgdetails=orgName,email&licenseDetails=name,description,url&fields=contentType,primaryCategory,topic,name,channel,mimeType,appIcon,gradeLevel,resourceType,identifier,medium,pkgVersion,board,subject,trackable,posterImage,duration,creatorLogo,license,version,versionKey&batchDetails=name,endDate,startDate,status,enrollmentType,createdBy,certificates`,
+  FETCH_USER_ENROLLMENT_LIST_PROFILE: (userId: string | undefined) =>
+    // tslint:disable-next-line: max-line-length
     `/apis/proxies/v8/learner/course/v1/user/enrollment/list/${userId}?orgdetails=orgName,email&licenseDetails=name,description,url&fields=contentType,primaryCategory,topic,name,channel,mimeType,appIcon,gradeLevel,resourceType,identifier,medium,pkgVersion,board,subject,trackable,posterImage,duration,creatorLogo,license,version,versionKey&batchDetails=name,endDate,startDate,status,enrollmentType,createdBy,certificates&retiredCoursesEnabled=true`,
   // tslint:disable-next-line: max-line-length
   FETCH_USER_ENROLLMENT_LIST_V2: (userId: string | undefined, orgdetails: string, licenseDetails: string, fields: string, batchDetails: string) =>
@@ -45,6 +48,24 @@ export class WidgetUserService {
       path = API_END_POINTS.FETCH_USER_ENROLLMENT_LIST_V2(userId, queryParams.orgdetails, queryParams.licenseDetails, queryParams.fields, queryParams.batchDetails)
     } else {
       path = API_END_POINTS.FETCH_USER_ENROLLMENT_LIST(userId)
+    }
+    return this.http
+      .get(path)
+      .pipe(
+        catchError(this.handleError),
+        map(
+          (data: any) => data.result.courses
+        )
+      )
+  }
+
+  fetchProfileUserBatchList(userId: string | undefined, queryParams?: { orgdetails: any, licenseDetails: any, fields: any, batchDetails: any }): Observable<NsContent.ICourse[]> {
+    let path = ''
+    if (queryParams) {
+       // tslint:disable-next-line: max-line-length
+      path = API_END_POINTS.FETCH_USER_ENROLLMENT_LIST_V2(userId, queryParams.orgdetails, queryParams.licenseDetails, queryParams.fields, queryParams.batchDetails)
+    } else {
+      path = API_END_POINTS.FETCH_USER_ENROLLMENT_LIST_PROFILE(userId)
     }
     return this.http
       .get(path)
