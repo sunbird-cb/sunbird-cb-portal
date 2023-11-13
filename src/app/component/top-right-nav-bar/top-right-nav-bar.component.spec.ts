2c7e5861cb8ee5523fb23cf6d08df30db9d6d8a4
vishnubansal <vishnu.bansal@tarento.com>
2023-11-13 15:04:29 +0530

    update for fine fixes for font setting download popup and transparent ui

---
+++ src/app/component/top-right-nav-bar/top-right-nav-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopRightNavBarComponent } from './top-right-nav-bar.component';
+
+describe('TopRightNavBarComponent', () => {
+  let component: TopRightNavBarComponent;
+  let fixture: ComponentFixture<TopRightNavBarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TopRightNavBarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TopRightNavBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
