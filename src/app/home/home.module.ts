9bb0dfb081a8721871b6667b22383d13129c1837
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2023-11-17 15:07:59 +0530

    trending discussion API is integrated

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,29 +7,31 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
 import { HeaderModule } from '../header/header.module';
-import { HomeComponent } from './home/home.component';
-import { InsightSideBarComponent } from '../component/in-sight-side-bar/in-sight-side-bar.component';
-import { DiscussionInfoComponent } from '../component/discussion-info/discussion-info.component';
-import { PageContainerComponent } from '../component/page-container/page-container.component';
 import {
   GridLayoutModule,  SlidersModule, DiscussStripMultipleModule, NetworkStripMultipleModule, ContentStripWithTabsModule
 } from '@sunbird-cb/collection';
 import { SkeletonLoaderModule } from '@sunbird-cb/collection/src/lib/_common/skeleton-loader/skeleton-loader.module';
+import { ProfileCardStatsModule } from '@sunbird-cb/collection/src/lib/_common/profile-card-stats/profile-card-stats.module';
+import { PipeRelativeTimeModule } from '@sunbird-cb/utils';
+import { WeeklyClapsModule } from '@sunbird-cb/collection/src/lib/_common/weekly-claps/weekly-claps.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { HomeComponent } from './home/home.component';
+import { InsightSideBarComponent } from '../component/in-sight-side-bar/in-sight-side-bar.component';
+import { DiscussionInfoComponent } from '../component/discussion-info/discussion-info.component';
+import { PageContainerComponent } from '../component/page-container/page-container.component';
 import { FeedListComponent } from './home/feed-list/feed-list.component';
-import { SkeletonLoaderComponent } from '../component/skeleton-loader/skeleton-loader.component';
-import { ProfileCardStatsModule } from '@sunbird-cb/collection/src/lib/_common/profile-card-stats/profile-card-stats.module'
 import { ClientSliderComponent } from '../component/client-slider/client-slider.component';
 import { HomeOtherPortalComponent } from '../component/home-other-portal/home-other-portal.component';
 import { NoDataComponent } from '../component/no-data/no-data.component';
-import { SharedModule } from '../shared/shared.module';
-import { WeeklyClapsModule } from '@sunbird-cb/collection/src/lib/_common/weekly-claps/weekly-claps.module';
-import { HomePageService } from '../services/home-page.service';
 import { HomeContainerComponent } from '../component/home-container/home-container.component.ts';
 import { DiscussHubComponent } from './home/discuss-hub/discuss-hub.component';
 import { NetworkHubComponent } from './home/network-hub/network-hub.component';
 
+import { HomePageService } from '../services/home-page.service';
+
 @NgModule({
-  declarations: [HomeComponent, FeedListComponent, InsightSideBarComponent, PageContainerComponent, DiscussionInfoComponent, SkeletonLoaderComponent, ClientSliderComponent, HomeOtherPortalComponent, NoDataComponent, HomeContainerComponent, DiscussHubComponent, NetworkHubComponent],
+  declarations: [HomeComponent, FeedListComponent, InsightSideBarComponent, PageContainerComponent, DiscussionInfoComponent, ClientSliderComponent, HomeOtherPortalComponent, NoDataComponent, HomeContainerComponent, DiscussHubComponent, NetworkHubComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -45,7 +47,8 @@ import { NetworkHubComponent } from './home/network-hub/network-hub.component';
     ProfileCardStatsModule,
     MatIconModule,
     WeeklyClapsModule,
-    SkeletonLoaderModule
+    SkeletonLoaderModule,
+    PipeRelativeTimeModule
   ],
   exports: [
     HeaderModule,
