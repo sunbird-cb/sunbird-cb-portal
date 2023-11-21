42555909a5a5ab2d7fb63996030521a41a43897a
Nandini Achutha <nandini.achutha@tarento.com>
2023-11-21 16:21:58 +0530

    Updated event cards UI & data

--- project/ws/app/src/lib/routes/events/components/today-event-card/today-event-card.component.ts
+++ project/ws/app/src/lib/routes/events/components/today-event-card/today-event-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { Router } from '@angular/router'
+import moment from 'moment'
 
 @Component({
   selector: 'ws-app-today-event-card',
@@ -13,6 +14,22 @@ export class TodayEventCardComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+    if(this.eventData) {
+      const eventDate = this.customDateFormat(this.eventData.startDate, this.eventData.startTime)
+      const eventendDate = this.customDateFormat(this.eventData.endDate, this.eventData.endTime)
+      const now = new Date()
+      const today = moment(now).format('YYYY-MM-DD HH:mm')
+
+      if (eventDate < today && eventendDate < today) {
+        this.isLive = false
+      }
+    }
+  }
+  customDateFormat(date: any, time: any) {
+    const stime = time.split('+')[0]
+    const hour = stime.substr(0, 2)
+    const min = stime.substr(2, 3)
+    return `${date} ${hour}${min}`
   }
   getEventDetails(eventID: any) {
     // this.router.navigate([`/app/event-hub/home/${this.discuss.tid}`])
