90a25f8ae0661f7c7766c166a3161147fb2d5a63
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2023-11-23 18:44:45 +0530

    discussion card css issue fixed

--- library/ws-widget/collection/src/lib/_common/discussions/discussions.component.ts
+++ library/ws-widget/collection/src/lib/_common/discussions/discussions.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'ws-discussions',
   templateUrl: './discussions.component.html',
-  styleUrls: ['./discussions.component.scss']
+  styleUrls: ['./discussions.component.scss'],
 })
 
 export class DiscussionsComponent implements OnInit{
 
   @Input("discussionObj") discussion: any;
   @Input("count") count: any;
+  @Input("trend") trend: boolean = false;
   countArr: any[] = [];
   dataToBind: any;
 
