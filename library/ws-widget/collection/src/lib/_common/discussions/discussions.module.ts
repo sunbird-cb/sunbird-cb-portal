1e4445781bce41f2cb102f33970b21d7416dd047
Saurav Verma <saurav.verma@tarento.com>
2023-11-27 09:06:03 +0530

    update posts pull

---
+++ library/ws-widget/collection/src/lib/_common/discussions/discussions.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material';
+import { SkeletonLoaderModule } from '../skeleton-loader/skeleton-loader.module';
+import { PipeRelativeTimeModule } from '@sunbird-cb/utils';
+
+import { DiscussionsComponent } from './discussions.component';
+
+@NgModule({
+  declarations: [DiscussionsComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    SkeletonLoaderModule,
+    PipeRelativeTimeModule,
+  ],
+  exports: [
+    DiscussionsComponent,
+  ],
+  entryComponents: [DiscussionsComponent],
+})
+
+export class DiscussionsModule { }
