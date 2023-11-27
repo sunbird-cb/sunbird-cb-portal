bd72f9fbce78dd3ccb3c6c96f944b96f0aeb2b34
Venky <venky.kandagaddala@gmail.com>
2023-11-27 17:00:51 +0530

    Knowledge resource

--- project/ws/app/src/lib/routes/knowledge-resource/routes/knowledge-saved/knowledge-saved.component.ts
+++ project/ws/app/src/lib/routes/knowledge-resource/routes/knowledge-saved/knowledge-saved.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router'
 import { KnowledgeResourceService } from '../../services/knowledge-resource.service'
 // tslint:disable
 import _ from 'lodash'
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core'
 // tslint:enable
 
 @Component({
@@ -18,9 +19,19 @@ export class KnowledgeSavedComponent implements OnInit {
   searchText = ''
   // kwResources: any;
   constructor(private activateRoute: ActivatedRoute,
-              private kwResources: KnowledgeResourceService
+              private kwResources: KnowledgeResourceService,
+              private translate: TranslateService,
     ) {
       this.filterSaved(null)
+      if (localStorage.getItem('websiteLanguage')) {
+        this.translate.setDefaultLang('en')
+        let lang = localStorage.getItem('websiteLanguage')!
+
+        this.translate.use(lang)
+        this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+          console.log('onLangChange', event);
+        });
+      }
   }
 
   ngOnInit() {
