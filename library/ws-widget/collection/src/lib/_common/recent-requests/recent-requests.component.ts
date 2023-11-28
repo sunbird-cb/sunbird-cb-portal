df4c5947a95387b115d1cc4e7c42141e12720b50
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2023-11-28 17:53:32 +0530

    changes in the issues

--- library/ws-widget/collection/src/lib/_common/recent-requests/recent-requests.component.ts
+++ library/ws-widget/collection/src/lib/_common/recent-requests/recent-requests.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { ConfigurationsService } from '@sunbird-cb/utils/src/public-api';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { ConfigurationsService } from '@sunbird-cb/utils/src/public-api'
 
 @Component({
   selector: 'ws-widget-recent-requests',
@@ -8,31 +8,31 @@ import { ConfigurationsService } from '@sunbird-cb/utils/src/public-api';
 })
 export class RecentRequestsComponent implements OnInit {
 
-  @Input() recentRequests: any;
-  @Output() updateRequest: EventEmitter<any> = new EventEmitter(); 
-  userInfo: any;
+  @Input() recentRequests: any
+  @Output() updateRequest: EventEmitter<any> = new EventEmitter()
+  userInfo: any
 
   constructor(
     private configService: ConfigurationsService
   ) { }
 
   ngOnInit() {
-    this.userInfo =  this.configService && this.configService.userProfile;
+    this.userInfo =  this.configService && this.configService.userProfile
   }
 
   handleRequest(reqObject: any, action: string): void {
     const payload = {
-      "userIdFrom": this.userInfo.userId,
-      "userNameFrom": this.userInfo.userId,
-      "userDepartmentFrom": this.userInfo.departmentName,
-      "userIdTo": reqObject.id,
-      "userNameTo": reqObject.id,
-      "userDepartmentTo": reqObject.departmentName,
-      "status": action
-    };
+      userIdFrom: this.userInfo.userId,
+      userNameFrom: this.userInfo.userId,
+      userDepartmentFrom: this.userInfo.departmentName,
+      userIdTo: reqObject.id,
+      userNameTo: reqObject.id,
+      userDepartmentTo: reqObject.departmentName,
+      status: action,
+    }
 
-    reqObject.connecting = true;
-    this.updateRequest.emit({payload, action, reqObject});
+    reqObject.connecting = true
+    this.updateRequest.emit({ payload, action, reqObject })
   }
 
 }
