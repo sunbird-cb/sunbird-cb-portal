c691d010b60c08d2112372b4d21b6fc65d4c9262
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2023-12-08 18:03:20 +0530

    increased the scroll size

--- project/ws/app/src/lib/routes/profile-v2/routes/profile-view/profile-view.component.ts
+++ project/ws/app/src/lib/routes/profile-v2/routes/profile-view/profile-view.component.ts
@@ -173,7 +173,7 @@ export class ProfileViewComponent implements OnInit, AfterViewInit, OnDestroy {
 
     if (this.selectedTabIndex) {
       window.scrollTo({
-        top: 380,
+        top: 420,
         behavior: 'smooth',
       })
     }
