71f4702a446e3415279bc9e925bb947446743235
nitinraj-tarento <nitin.raj@tarento.com>
2023-12-11 15:49:56 +0530

    form ui fix

--- project/ws/app/src/lib/routes/user-profile/components/user-profile/user-profile.component.ts
+++ project/ws/app/src/lib/routes/user-profile/components/user-profile/user-profile.component.ts
@@ -63,8 +63,6 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   createUserForm: FormGroup
   unseenCtrl!: FormControl
   unseenCtrlSub!: Subscription
-  masterGroup: any = []
-  groupsOriginal: any = []
   uploadSaveData = false
   selectedIndex = 0
   masterNationality: Observable<INation[]> | undefined
@@ -231,13 +229,13 @@ export class UserProfileComponent implements OnInit, OnDestroy {
     if (approvalData.length > 0) {
       // need to call search API
     }
-    console.log( this.masterGroup, "this.masterGroup")
+
 
     this.getUserDetails()
     this.init()
     this.checkIfMobileNoChanged()
     this.onPhoneChange()
-    this.onGroupChange()
+
   }
 
   displayFnPosition = (value: any) => {
@@ -272,23 +270,6 @@ export class UserProfileComponent implements OnInit, OnDestroy {
       (_err: any) => {
       })
 
-      this.userProfileSvc.getGroups().subscribe(
-        (data) => {
-          console.log(data, 'data--==========')
-          console.log(data.result.response, "data.result.response=")
-          debugger
-          data.result.response.map((value: any) => {
-            
-            console.log(value, "value--")
-            this.masterGroup.push(value)
-           
-            console.log(this.masterGroup, "this.masterGroup===")
-          })
-          this.onGroupChange()
-          console.log(data, 'group data==')
-        },
-        (_err: any) => {
-        })
 
     this.userProfileSvc.getMasterNationlity().subscribe(
       data => {
@@ -370,31 +351,8 @@ export class UserProfileComponent implements OnInit, OnDestroy {
       })
   }
 
-    onGroupChange() {
-    // tslint:disable-next-line: no-non-null-assertion
-    this.masterGroup = this.createUserForm.get('group')!.valueChanges
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        startWith(''),
-        map((value: any) => typeof (value) === 'string' ? value : (value && value.name ? value.name : '')),
-        map((name: any) => name ? this.filterGroups(name) : this.groupsOriginal.slice())
-      )
 
-    this.masterGroup.subscribe(() => {
-      // tslint:disable-next-line: no-non-null-assertion
-      this.createUserForm.get('group')!.setValidators([Validators.required])
-      this.createUserForm.updateValueAndValidity()
-    })
-  }
 
-    private filterGroups(name: string): any {
-    if (name) {
-      const filterValue = name.toLowerCase()
-      return this.groupsOriginal.filter((option: any) => option.toLowerCase().includes(filterValue))
-    }
-    return this.groupsOriginal
-  }
 
   emailVerification(emailId: string) {
     this.emailLengthVal = false
