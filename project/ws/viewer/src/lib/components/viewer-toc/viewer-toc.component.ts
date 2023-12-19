15948d1c953d3f6943f958222fb52e0dfe93215a
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2023-12-19 12:11:11 +0530

    log check

--- project/ws/viewer/src/lib/components/viewer-toc/viewer-toc.component.ts
+++ project/ws/viewer/src/lib/components/viewer-toc/viewer-toc.component.ts
@@ -118,6 +118,8 @@ export class ViewerTocComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.hierarchyData = this.activatedRoute.snapshot.data.hierarchyData && this.activatedRoute.snapshot.data.hierarchyData.data || ''
     this.enrollmentList = this.activatedRoute.snapshot.data.enrollmentData && this.activatedRoute.snapshot.data.enrollmentData.data || ''
+    // tslint:disable-next-line
+    console.log(this.hierarchyData,'hierarchyData')
     if (this.configSvc.instanceConfig && this.configSvc.instanceConfig.logos) {
       const logo = this.configSvc.instanceConfig.logos.defaultContent || ''
       this.defaultThumbnail = this.domSanitizer.bypassSecurityTrustResourceUrl(logo)
