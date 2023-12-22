876b763c8ef871859706a8629f9b86bf93599b2f
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2023-12-22 07:56:26 +0530

    working on integration

--- src/app/competency-passbook/competency-list/competency-list.component.ts
+++ src/app/competency-passbook/competency-list/competency-list.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
+import { CompetencyPassbookService } from '../competency-passbook.service';
 
 @Component({
   selector: 'ws-competency-list',
   templateUrl: './competency-list.component.html',
   styleUrls: ['./competency-list.component.scss']
 })
+
 export class CompetencyListComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    private cpService: CompetencyPassbookService
+  ) { }
 
   ngOnInit() {
+
+    this.fetchCompetencyList()
+  }
+
+  fetchCompetencyList(): void {
+    const payload = {
+      "search": {
+        "type": "Competency Area"
+      },
+      "filter": {
+        "isDetail": true
+      }
+    };
+
+    this.cpService.getCompetencyList(payload).subscribe(
+      (_res: any) => {
+        console.log('res - ', _res);
+      }, (error: HttpErrorResponse) => {
+        if (!error.ok) {
+          alert('Some issue!');
+        }
+      }
+    )
   }
 
 }
