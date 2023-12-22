919acde3c6194040327f9eadc69e9fc2c4b7aaba
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2023-12-22 08:55:24 +0530

    changes in orgainzation

--- library/ws-widget/collection/src/lib/content-strip-with-tabs/content-strip-with-tabs.component.ts
+++ library/ws-widget/collection/src/lib/content-strip-with-tabs/content-strip-with-tabs.component.ts
@@ -271,11 +271,11 @@ export class ContentStripWithTabsComponent extends WidgetBaseComponent
     } else if (filters && filters.hasOwnProperty('batches.enrollmentEndDate')) {
       // tslint:disable-next-line
       filters['batches.enrollmentEndDate']['>='] = eval(filters['batches.enrollmentEndDate']['>='])
-    } else if(filters.organisation &&
+    } else if (filters.organisation &&
       filters.organisation.indexOf('<orgID>') >= 0
     ) {
       filters['organisation'] = userData && userData.rootOrgId
-    
+
     if (filters && filters.hasOwnProperty('designation')) {
       filters['designation'] = userData.professionalDetails.length > 0 ?
        userData.professionalDetails[0].designation : ''
