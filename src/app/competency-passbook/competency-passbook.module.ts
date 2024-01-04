6de3cdd9ab911e20c369dda4858bb1d2644a05f8
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2024-01-04 10:36:43 +0530

    working on filters inclusion in passbook

--- src/app/competency-passbook/competency-passbook.module.ts
+++ src/app/competency-passbook/competency-passbook.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MatIconModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatCheckboxModule,
+  MatTabsModule,
+  MatBottomSheetModule,
+  MatMenuModule,
+  MatRadioModule
+} from '@angular/material'
 
 import { SkeletonLoaderModule } from '@sunbird-cb/collection/src/lib/_common/skeleton-loader/skeleton-loader.module';
 import { PipeSafeSanitizerModule } from '@sunbird-cb/utils/src/public-api';
@@ -14,6 +21,7 @@ import { CompetencyListComponent } from './competency-list/competency-list.compo
 import { CompetencyCardDetailsComponent } from './competency-card-details/competency-card-details.component';
 import { CompetencyPassbookSideBarComponent } from './../component/competency-passbook-side-bar/competency-passbook-side-bar.component';
 import { CompetencySearchComponent } from './competency-search/competency-search.component';
+// import { FilterComponent } from '../component/filter/filter.component';
 
 @NgModule({
   declarations: [CompetencyPassbookComponent, CompetencyPassbookSideBarComponent, CompetencySearchComponent, CompetencyListComponent, CompetencyCardDetailsComponent],
@@ -23,6 +31,11 @@ import { CompetencySearchComponent } from './competency-search/competency-search
     MatIconModule,
     MatMenuModule,
     MatTabsModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatBottomSheetModule,
+    MatRadioModule,
     CompetencyPassbookRoutingModule,
     SkeletonLoaderModule,
     PipeSafeSanitizerModule
