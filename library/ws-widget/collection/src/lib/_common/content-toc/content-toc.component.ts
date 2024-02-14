3c73548d603511214d625f7f0b2fdd7613280f70
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2024-02-14 16:43:59 +0530

    after fixing lint and build errors

--- library/ws-widget/collection/src/lib/_common/content-toc/content-toc.component.ts
+++ library/ws-widget/collection/src/lib/_common/content-toc/content-toc.component.ts
@@ -14,7 +14,7 @@ export class ContentTocComponent implements OnInit, AfterViewInit, OnChanges {
 
   tabChangeValue: any = ''
   @Input() content: NsContent.IContent | null = null
-  @Input() initialrouteData: any
+  @Input() initialRouteData: any
   @Input() changeTab = false
   routeSubscription: Subscription | null = null
   @Input() forPreview = window.location.href.includes('/public/') || window.location.href.includes('&preview=true')
