edf15efa9eda739ed4e921a246b0c693ed2a9763
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2024-03-05 14:15:54 +0530

    after fixing lint errors

--- library/ws-widget/collection/src/lib/_common/content-toc/karma-points/karma-points.component.ts
+++ library/ws-widget/collection/src/lib/_common/content-toc/karma-points/karma-points.component.ts
@@ -39,6 +39,7 @@ export class KarmaPointsComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     this.condition = changes.condition.currentValue
 
+    if (!this.condition) { return }
     if (!this.condition.isPostAssessment && (this.condition.content && this.condition.content.hasOwnProperty('completionPercentage')
      && !this.condition.content.completionPercentage
      || (this.condition.content && this.condition.content.completionPercentage < 100))
