8d827cd1242f901d1369c04aa05d65a7220514a0
Venky <venky.kandagaddala@gmail.com>
2024-03-11 15:42:03 +0530

    Added title and Cards responsive

--- project/ws/app/src/lib/routes/jan-karmayogi/jan-karmayogi.module.ts
+++ project/ws/app/src/lib/routes/jan-karmayogi/jan-karmayogi.module.ts
@@ -5,6 +5,7 @@ import { JanKarmayogiRoutingModule } from './jan-karmayogi-routing.module'
 import { JanKarmayogiHomeComponent } from './components/jan-karmayogi-home/jan-karmayogi-home.component'
 import { WidgetResolverModule } from '@sunbird-cb/resolver/src/public-api'
 import { CardContentV2Module } from '@sunbird-cb/collection/src/lib/card-content-v2/card-content-v2.module'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
   declarations: [JanKarmayogiHomeComponent],
@@ -13,6 +14,7 @@ import { CardContentV2Module } from '@sunbird-cb/collection/src/lib/card-content
     JanKarmayogiRoutingModule,
     WidgetResolverModule,
     CardContentV2Module,
+    TranslateModule,
   ],
 })
 export class JanKarmayogiModule { }
