21e567e957c05244d444872e70e3adbd17d1659d
Vinod Kumar Shyave <121538586+vShyave@users.noreply.github.com>
2024-03-12 09:45:57 +0530

    KB-3226 is fixed

--- project/ws/app/src/lib/routes/app-toc/components/app-toc-home/app-toc-home.component.ts
+++ project/ws/app/src/lib/routes/app-toc/components/app-toc-home/app-toc-home.component.ts
@@ -188,7 +188,7 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
   isCompletedThisMonth = false
   @ViewChild('rightContainer', { static: false }) rcElement!: ElementRef
   @ViewChild('bannerDetails', { static: true }) bannerElem!: ElementRef
-  @ViewChild('contentSource', { static: true }) contentSource!: ElementRef
+  @ViewChild('contentSource', { static: false }) contentSource!: ElementRef
   sourceEllipsis = false
   scrollLimit = 0
   rcElem = {
@@ -289,7 +289,9 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
         }
       }
     })
+
     this.getServerDateTime()
+
     this.selectedBatchSubscription = this.tocSvc.getSelectedBatch.subscribe(batchData => {
       this.selectedBatchData = batchData
     })
@@ -297,7 +299,7 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
     this.serverDateSubscription = this.tocSvc.serverDate.subscribe(serverDate => {
       this.serverDate = serverDate
     })
-    // this.route.fragment.subscribe(fragment => { this.fragment = fragment })
+
     this.channelId = this.telemetryService.telemetryConfig ? this.telemetryService.telemetryConfig.channel : ''
     try {
       this.isInIframe = window.self !== window.top
@@ -306,16 +308,15 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
     }
 
     if (this.route) {
+      this.skeletonLoader = true
       this.routeSubscription = this.route.data.subscribe((data: Data) => {
         if (data && data.content && data.content.data && data.content.data.identifier) {
           this.courseID = data.content.data.identifier
-          this.skeletonLoader = true
+          this.skeletonLoader = false
           this.tocSvc.fetchGetContentData(data.content.data.identifier).subscribe(res => {
             this.contentReadData = res.result.content
-            this.skeletonLoader = false
           },                                                                      (error: HttpErrorResponse) => {
             if (!error.ok) {
-              this.skeletonLoader = false
               this.matSnackBar.open('Unable to fetch content data, due to some error!')
             }
           })
@@ -427,6 +428,7 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
         primaryCategory: this.content.primaryCategory,
       }
     }
+
     if (this.content && (
       this.content.primaryCategory === this.primaryCategory.COURSE ||
       this.content.primaryCategory === this.primaryCategory.STANDALONE_ASSESSMENT ||
@@ -813,14 +815,6 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
     })
 
     this.tocSvc.contentLoader.next(false)
-
-    // setTimeout(() => {
-    //   console.log("this.contentSource - ", this.contentSource);
-    //   if (this.contentSource && this.contentSource.nativeElement.offsetHeight > 44) {
-    //     console.log("if this.contentSource - ", this.contentSource.nativeElement.offsetHeight);
-    //     this.sourceEllipsis = true
-    //   }
-    // }, 250)
   }
 
   getUserRating(fireUpdate: boolean) {
@@ -845,6 +839,12 @@ export class AppTocHomeComponent implements OnInit, OnDestroy, AfterViewChecked,
       }
     }
 
+    setTimeout(() => {
+      console.log('this.contentSource - ', this.contentSource)
+      if (this.contentSource && this.contentSource.nativeElement.offsetHeight > 44) {
+        this.sourceEllipsis = true
+      }
+    },         250)
   }
 
    private getUserEnrollmentList() {
