c6ddf3177106ec8ca00a394fea3fdc32645cc1e0
vishnubansal <vishnu.bansal@tarento.com>
2024-03-19 08:54:15 +0530

    update for submissionSurvey

--- src/app/component/survey-poc/survey-poc.component.ts
+++ src/app/component/survey-poc/survey-poc.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { environment } from 'src/environments/environment'
-import  {  ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router'
 import { DomSanitizer } from '@angular/platform-browser'
 @Component({
   selector: 'ws-survey-poc',
@@ -9,13 +9,15 @@ import { DomSanitizer } from '@angular/platform-browser'
 })
 export class SurveyPocComponent implements OnInit {
   hostUrl = environment.azureHost
-  solutionId:any
-  iframeUrl:any;
-  constructor(public route: ActivatedRoute,private sanitized: DomSanitizer) { }
+  solutionId: any
+  iframeUrl: any
+  constructor(public route: ActivatedRoute, private sanitized: DomSanitizer) { }
 
   ngOnInit() {
-    this.solutionId = this.route.snapshot.paramMap.get('id');
-    this.iframeUrl =  this.sanitized.bypassSecurityTrustResourceUrl(this.hostUrl+'/mligot/mlsurvey/'+this.solutionId);
+    this.solutionId = this.route.snapshot.paramMap.get('id')
+    const subUrl = '/mligot/mlsurvey/'
+    const url = `${this.hostUrl}${subUrl}${this.solutionId}`
+    this.iframeUrl =  this.sanitized.bypassSecurityTrustResourceUrl(url)
   }
 
 }
